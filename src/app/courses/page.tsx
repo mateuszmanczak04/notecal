97d887eab92305b43a81addb6e227afea1d3a197
Mateusz Mańczak <mateuszmanczak16@gmail.com>
2024-03-10 12:34:36 +0100

    create mobile courses page prototype

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -1,5 +1,49 @@
+const Course = ({ title }: { title: string }) => {
+	return (
+		<div className='bg-blue-700 text-white w-full flex flex-col font-semibold text-lg rounded-md p-4'>
+			<p>{title}</p>
+			<div className='flex w-full justify-between mt-2 gap-2'>
+				<button className='bg-white/10 h-8 px-3 rounded-md flex-1'>
+					Notes
+				</button>
+				<button className='bg-white/10 h-8 px-3 rounded-md flex-1'>Edit</button>
+				<button className='bg-white/10 h-8 px-3 rounded-md flex-1'>
+					Delete
+				</button>
+			</div>
+		</div>
+	);
+};
+
+const COURSES = [
+	{
+		id: 1,
+		title: 'Course 1',
+	},
+	{
+		id: 2,
+		title: 'Course 2',
+	},
+	{
+		id: 3,
+		title: 'Course 3',
+	},
+];
+
 const page = () => {
-	return <div>Courses Page</div>;
+	return (
+		<div className='p-4'>
+			<h1 className='text-2xl font-bold'>Your Courses:</h1>
+			<div className='flex flex-col gap-2 mt-2'>
+				<button className='bg-gray-100 hover:bg-gray-200 w-full h-12 grid place-content-center font-semibold text-lg rounded-md'>
+					+ Create a New Course
+				</button>
+				{COURSES.map(course => (
+					<Course key={course.id} {...course} />
+				))}
+			</div>
+		</div>
+	);
 };
 
 export default page;
