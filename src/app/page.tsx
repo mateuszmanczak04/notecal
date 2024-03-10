781e4a051e98bb81a3defd11f195d3c62e8c0c7d
Mateusz Mańczak <mateuszmanczak16@gmail.com>
2024-03-10 15:57:32 +0100

    make landing page responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,15 @@ import Image from 'next/image';
 export default function Home() {
 	return (
 		<div className='mx-auto mt-16 w-[600px] max-w-[calc(100%-32px)] items-center text-center'>
-			<p className='text-xl font-semibold'>NoteCal</p>
-			<h1 className='mt-2 text-5xl font-bold'>
+			<p className='text-xl font-semibold sm:text-2xl'>NoteCal</p>
+			<h1 className='mt-2 text-5xl font-bold sm:text-7xl'>
 				Let Your University Notes Be Organised
 			</h1>
-			<p className='mt-4'>
+			<p className='mt-4 sm:text-lg'>
 				Are you tired of grouping your university notes by a specific course
 				lesson or date? NoteCal has been made to decrease your fatigue.
 			</p>
-			<button className='bg-primary mt-8 h-10 w-full rounded-md px-3 text-white'>
+			<button className='bg-primary mt-8 h-10 w-full rounded-md px-6 text-white sm:h-12 sm:w-fit'>
 				Click Here to Learn More
 			</button>
 		</div>
