d226e5ca69ca7bd3024601eaa252c29c059e4922
Mateusz Mańczak <mateuszmanczak16@gmail.com>
2024-03-10 16:33:57 +0100

    create reusable button component with cva

---
+++ src/components/Button.tsx
@@ -0,0 +1,44 @@
+import cn from '@/utils/cn';
+import { VariantProps, cva } from 'class-variance-authority';
+import { ButtonHTMLAttributes, FC, forwardRef } from 'react';
+
+const buttonVariants = cva('transition rounded-md font-medium', {
+	variants: {
+		variant: {
+			primary: 'bg-primary hover:bg-primary/90 text-white',
+			secondary: 'bg-gray-200 text-black hover:bg-gray-300',
+		},
+		size: {
+			small: 'h-8 px-2',
+			medium: 'h-10 px-3',
+			large: 'h-12 px-4',
+		},
+	},
+	defaultVariants: {
+		variant: 'primary',
+		size: 'medium',
+	},
+});
+
+export interface ButtonProps
+	extends ButtonHTMLAttributes<HTMLButtonElement>,
+		VariantProps<typeof buttonVariants> {
+	ref?: React.Ref<HTMLButtonElement>;
+}
+
+const Button: FC<ButtonProps> = forwardRef(
+	({ children, className, size, variant, ...props }, ref) => {
+		return (
+			<button
+				ref={ref}
+				className={cn(buttonVariants({ className, variant, size }))}
+				{...props}>
+				{children}
+			</button>
+		);
+	},
+);
+
+Button.displayName = 'Button';
+
+export default Button;
