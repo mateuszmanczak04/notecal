adea3db52d8c7a831f0f02e62cec8b73b74875d7
Mateusz Mańczak <mateuszmanczak16@gmail.com>
2024-03-10 11:42:47 +0100

    make navigation fixed

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -6,7 +6,7 @@ interface NavigationProps {}
 const Navigation: FC<NavigationProps> = ({}) => {
 	return (
 		<div>
-			<div className='flex justify-between items-center w-full p-4 bg-gray-100'>
+			<div className='flex justify-between items-center w-full p-4 bg-gray-100 fixed top-0 left-0 w-scren z-50 h-16'>
 				<Link href='/' className='text-2xl font-semibold'>
 					NoteCal
 				</Link>
