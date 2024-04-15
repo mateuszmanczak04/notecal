6fbe2c476a7b0591c56bfced8582fc461ec9ae8f
Mateusz Mańczak <mateuszmanczak@icloud.com>
2024-04-15 08:01:08 +0200

    fix editing course and add initial form values there

--- src/app/courses/edit/page.tsx
+++ src/app/courses/edit/page.tsx
@@ -1,109 +1,34 @@
-'use client';
+import EditCourse from '@/components/edit-course';
+import Course from '@/models/Course';
+import dbConnect from '@/utils/dbConnect';
+import { redirect } from 'next/navigation';
+import { FC } from 'react';
 
-import editCourse from '@/actions/editCourse';
-import { Button } from '@/components/ui/button';
-import {
-	Form,
-	FormControl,
-	FormDescription,
-	FormField,
-	FormItem,
-	FormLabel,
-	FormMessage,
-} from '@/components/ui/form';
-import { Input } from '@/components/ui/input';
-import { EditCourseFormSchema } from '@/schemas';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { Trash2 } from 'lucide-react';
-import Link from 'next/link';
-import { useRouter, useSearchParams } from 'next/navigation';
-import { useEffect, useState, useTransition } from 'react';
-import { useForm } from 'react-hook-form';
-import { ClipLoader } from 'react-spinners';
-import { z } from 'zod';
+interface EditCoursePageProps {
+	searchParams?: { [key: string]: string | string[] | undefined };
+	params?: { [key: string]: string | undefined };
+}
 
-const EditCoursePage = () => {
-	const searchParams = useSearchParams();
-	const [isPending, startTransition] = useTransition();
-	const [error, setError] = useState('');
-	const router = useRouter();
-	const form = useForm<z.infer<typeof EditCourseFormSchema>>({
-		resolver: zodResolver(EditCourseFormSchema),
-		defaultValues: {
-			newName: '',
-			newTeacher: '',
-		},
-	});
+const EditCoursePage: FC<EditCoursePageProps> = async ({
+	searchParams,
+	params,
+}) => {
+	const id = searchParams?.id;
 
-	useEffect(() => {
-		const courseId = searchParams.get('id');
-		if (!courseId || typeof courseId !== 'string') {
-			router.replace('/courses');
-		} else {
-			form.setValue('id', courseId);
-		}
-	}, [searchParams, form, router]);
+	if (!id || typeof id !== 'string') {
+		redirect('/courses');
+	}
 
-	const onSubmit = (values: z.infer<typeof EditCourseFormSchema>) => {
-		console.log('wow');
-		console.log(values);
-		setError('');
-		startTransition(() => {
-			editCourse(values)
-				.then(res => console.log(res))
-				.catch(err => console.log(err));
-		});
-	};
+	await dbConnect();
+
+	const course = await Course.findOne({ _id: id });
+
+	console.log(course);
 
 	return (
-		<div>
-			<Form {...form}>
-				<form onSubmit={form.handleSubmit(onSubmit)} className='space-y-2'>
-					<FormField
-						control={form.control}
-						name='newName'
-						render={({ field }) => (
-							<FormItem>
-								<FormLabel>New name</FormLabel>
-								<FormControl>
-									<Input placeholder='Computer Science' {...field} />
-								</FormControl>
-								<FormDescription />
-								<FormMessage />
-							</FormItem>
-						)}
-					/>
-					<FormField
-						control={form.control}
-						name='newTeacher'
-						render={({ field }) => (
-							<FormItem>
-								<FormLabel>New teacher</FormLabel>
-								<FormControl>
-									<Input placeholder='John Doe' {...field} />
-								</FormControl>
-								<FormDescription />
-								<FormMessage />
-							</FormItem>
-						)}
-					/>
-					<Button type='submit' className='w-full'>
-						Save changes
-					</Button>
-					<div className='flex w-full justify-center'>
-						{isPending && <ClipLoader className='mx-auto' />}
-					</div>
-					{/* todo - display potential error */}
-				</form>
-			</Form>
-			<Button asChild variant='destructive' className='w-full gap-1'>
-				<Link
-					href={`/courses/delete?id=${form.getValues().id}&name=${'todo - put the name here'}`}>
-					<Trash2 className='h-4 w-4' />
-					Delete Course
-				</Link>
-			</Button>
-		</div>
+		<EditCourse
+			course={{ id: course._id, name: course.name, teacher: course.teacher }}
+		/>
 	);
 };
 
