f45f966ee81b75eec25f7f942733903f7ffb9668
Mateusz Mańczak <mateuszmanczak@icloud.com>
2024-04-25 18:36:55 +0200

    fix not allowing to create a task with past date

--- src/components/tasks/create-task-form.tsx
+++ src/components/tasks/create-task-form.tsx
@@ -217,7 +217,6 @@ const CreateTaskForm = () => {
 										mode='single'
 										selected={field.value}
 										onSelect={field.onChange}
-										disabled={date => date < new Date()}
 										initialFocus
 									/>
 								</PopoverContent>
