5417d7ba83bdb182e9a43d9cf28df1ffd11182cb
Mateusz Mańczak <mateuszmanczak@icloud.com>
2024-04-25 12:53:10 +0200

    fix clicking under select in sort-tasks component

--- src/components/tasks/sort-tasks.tsx
+++ src/components/tasks/sort-tasks.tsx
@@ -2,40 +2,67 @@
 
 import { getTasks } from '@/actions/get-tasks';
 import {
-	Select,
-	SelectContent,
-	SelectItem,
-	SelectTrigger,
-	SelectValue,
-} from '@/components/ui/select';
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuLabel,
+	DropdownMenuRadioGroup,
+	DropdownMenuRadioItem,
+	DropdownMenuSeparator,
+	DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
 import { useQueryClient } from '@tanstack/react-query';
+import { Button } from '../ui/button';
 
 const SortTasks = ({}) => {
 	const queryClient = useQueryClient();
 
-	const onSortChange = (
-		value: 'title' | 'createdAt' | 'dueDate' | 'priority' | 'completed',
-	) => {
-		if (!value) return;
-		queryClient.fetchQuery({
-			queryKey: ['tasks'],
-			queryFn: async () => await getTasks({ orderBy: value }),
-		});
+	const onSortChange = (value: string) => {
+		if (
+			value &&
+			(value === 'title' ||
+				value === 'createdAt' ||
+				value === 'dueDate' ||
+				value === 'priority' ||
+				value === 'completed')
+		) {
+			queryClient.fetchQuery({
+				queryKey: ['tasks'],
+				queryFn: async () => await getTasks({ orderBy: value }),
+			});
+		}
 	};
 
 	return (
-		<Select onValueChange={onSortChange}>
-			<SelectTrigger className='sm:flex-1'>
-				<SelectValue placeholder='Order by' />
-			</SelectTrigger>
-			<SelectContent className='md:flex-1'>
-				<SelectItem value='title'>Alphabetically</SelectItem>
-				<SelectItem value='dueDate'>By due date</SelectItem>
-				<SelectItem value='priority'>By priority</SelectItem>
-				<SelectItem value='createdAt'>Newest first</SelectItem>
-				<SelectItem value='completed'>Completed first</SelectItem>
-			</SelectContent>
-		</Select>
+		<DropdownMenu>
+			<DropdownMenuTrigger className='-mt-1 h-6 w-full flex-1 -translate-y-1 select-none pt-0 outline-none'>
+				<Button variant='outline' className='w-full'>
+					Order By
+				</Button>
+			</DropdownMenuTrigger>
+			<DropdownMenuContent>
+				<DropdownMenuLabel>Order by</DropdownMenuLabel>
+				<DropdownMenuSeparator />
+				<DropdownMenuRadioGroup
+					defaultValue='title'
+					onValueChange={onSortChange}>
+					<DropdownMenuRadioItem value='title' className='cursor-pointer'>
+						Title
+					</DropdownMenuRadioItem>
+					<DropdownMenuRadioItem value='dueDate' className='cursor-pointer'>
+						Due date
+					</DropdownMenuRadioItem>
+					<DropdownMenuRadioItem value='priority' className='cursor-pointer'>
+						Priority
+					</DropdownMenuRadioItem>
+					<DropdownMenuRadioItem value='createdAt' className='cursor-pointer'>
+						Newest first
+					</DropdownMenuRadioItem>
+					<DropdownMenuRadioItem value='completed' className='cursor-pointer'>
+						Completed first
+					</DropdownMenuRadioItem>
+				</DropdownMenuRadioGroup>
+			</DropdownMenuContent>
+		</DropdownMenu>
 	);
 };
 
