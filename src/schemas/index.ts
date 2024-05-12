a42d71363858967af2d1bec6de62b4c8b2656af2
Mateusz Mańczak <mateuszmanczak@icloud.com>
2024-05-12 10:56:17 +0200

    fix createTask description optional type

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -43,12 +43,9 @@ export const CreateTaskFormSchema = z.object({
 	title: z.string().min(1, { message: 'Task title is required.' }).max(60, {
 		message: 'Max length is 60 characters.',
 	}),
-	description: z
-		.string()
-		.max(300, {
-			message: 'Max length is 300 characters',
-		})
-		.optional(),
+	description: z.string().max(300, {
+		message: 'Max length is 300 characters',
+	}),
 	courseId: z.string(),
 	priority: z.enum([TaskPriority.A, TaskPriority.B, TaskPriority.C]).nullable(),
 	dueDate: z.date().nullable(),
