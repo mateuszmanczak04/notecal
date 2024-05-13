72778bc5de8145283ee09a03a9c735c52c88a522
Mateusz Mańczak <mateuszmanczak@icloud.com>
2024-05-13 12:03:25 +0200

    block creating notes after clicking on another note in calendar

--- src/components/calendar/day-grid.tsx
+++ src/components/calendar/day-grid.tsx
@@ -2,20 +2,40 @@
 
 import { useCalendarContext } from '@/components/calendar/calendar-context';
 import useCourses from '@/hooks/use-courses';
-import { FC, MouseEvent } from 'react';
+import { FC, MouseEvent, useRef } from 'react';
 
 interface CalendarDayGridProps {
 	date: Date;
 }
 
-const GridRect = ({ last = false }: { last?: boolean }) => {
-	if (last) return <div className='h-16 w-full border-y border-gray-300'></div>;
-	return <div className='h-16 w-full border-t border-gray-300'></div>;
+const GridRect = ({
+	last = false,
+	hour,
+	onClick,
+}: {
+	last?: boolean;
+	hour: number;
+	onClick: (e: MouseEvent<HTMLDivElement>) => void;
+}) => {
+	if (last)
+		return (
+			<div
+				className='h-16 w-full border-y border-gray-300'
+				data-hour={hour}
+				onClick={onClick}></div>
+		);
+	return (
+		<div
+			className='h-16 w-full border-t border-gray-300'
+			data-hour={hour}
+			onClick={onClick}></div>
+	);
 };
 
 const CalendarDayGrid: FC<CalendarDayGridProps> = ({ date }) => {
 	const { notes, addNewNote } = useCalendarContext();
 	const { data: coursesData } = useCourses();
+	const gridRef = useRef<HTMLDivElement | null>(null);
 
 	const todayNotes = notes.filter(note => {
 		const startTime = note.startTime;
@@ -24,8 +44,7 @@ const CalendarDayGrid: FC<CalendarDayGridProps> = ({ date }) => {
 
 	const onAddNewNote = (e: MouseEvent<HTMLDivElement>) => {
 		const startTime = new Date(date);
-		const hoursPercentage = (e.nativeEvent.layerY + 40) / 1536;
-		const hours = Math.floor(hoursPercentage * 24);
+		const hours = parseInt(e.currentTarget.getAttribute('data-hour') || '0');
 		const minutesPercentage = ((e.nativeEvent.layerY + 40) % 64) / 64;
 		let minutes = Math.floor(minutesPercentage * 60);
 		minutes = minutes - (minutes % 15);
@@ -44,12 +63,12 @@ const CalendarDayGrid: FC<CalendarDayGridProps> = ({ date }) => {
 
 	return (
 		<div
-			className='relative flex-1 cursor-crosshair border-r border-gray-300'
-			onClick={onAddNewNote}>
+			ref={gridRef}
+			className='relative flex-1 cursor-crosshair border-r border-gray-300'>
 			{new Array(23).fill(0).map((_, i) => (
-				<GridRect key={i} />
+				<GridRect key={i} hour={i} onClick={onAddNewNote} />
 			))}
-			<GridRect last />
+			<GridRect last hour={23} onClick={onAddNewNote} />
 			{/* notes: */}
 			{todayNotes.map(note => {
 				const hour = note.startTime.getHours();
@@ -57,7 +76,7 @@ const CalendarDayGrid: FC<CalendarDayGridProps> = ({ date }) => {
 				const topTranslate = Math.floor((hour + minute / 60) * 64);
 				return (
 					<div
-						className='absolute left-1 right-1 top-0 h-16 cursor-pointer select-none overflow-y-hidden rounded-md bg-blue-500 bg-opacity-10 p-2'
+						className='absolute left-2 right-0 top-0 h-16 cursor-pointer select-none overflow-y-hidden rounded-md bg-blue-500 bg-opacity-10 p-2'
 						style={{ transform: `translateY(${topTranslate}px)` }}
 						key={note.id}>
 						{coursesData?.courses?.find(c => c.id === note.courseId)?.name ||
