59e0ac778db0cad2e54f401c0fa48c03f2a32a7d
Mateusz Mańczak <mateuszmanczak@icloud.com>
2024-06-06 19:28:45 +0200

    add "go back" button in edit course page

--- src/app/courses/edit/page.tsx
+++ src/app/courses/edit/page.tsx
@@ -1,7 +1,11 @@
+'use client';
+
 import DeleteCourseLink from '@/components/courses/delete-course-link';
 import EditCourseForm from '@/components/courses/edit-course-form';
+import { Button } from '@/components/ui/button';
 import { Separator } from '@/components/ui/separator';
-import { redirect } from 'next/navigation';
+import { ChevronLeft, MoveLeft } from 'lucide-react';
+import { redirect, useRouter } from 'next/navigation';
 import { FC } from 'react';
 
 interface EditCoursePageProps {
@@ -10,17 +14,24 @@ interface EditCoursePageProps {
 
 const EditCoursePage: FC<EditCoursePageProps> = ({ searchParams }) => {
 	const id = searchParams?.id;
+	const router = useRouter();
 
 	if (!id || typeof id !== 'string') {
 		redirect('/courses');
 	}
 
 	return (
-		<>
+		<div className='flex flex-col gap-4'>
+			<Button
+				variant='secondary'
+				onClick={() => router.back()}
+				className='flex items-center gap-1'>
+				<MoveLeft className='h-5' />
+				Go back
+			</Button>
 			<EditCourseForm id={id} />
-			<Separator className='my-8' />
 			<DeleteCourseLink id={id} />
-		</>
+		</div>
 	);
 };
 
