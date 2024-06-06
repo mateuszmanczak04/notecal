4c8ab75722eb03d1b7772fb9a33368a591377c99
Mateusz Mańczak <mateuszmanczak@icloud.com>
2024-06-06 09:41:11 +0200

    create theme setting draft component

--- src/components/settings/theme-setting.tsx
+++ src/components/settings/theme-setting.tsx
@@ -1,11 +1,21 @@
 'use client';
 
 import { FC } from 'react';
+import { Switch } from '@/components/ui/switch';
+import { Label } from '@/components/ui/label';
 
 interface ThemeSettingProps {}
 
 const ThemeSetting: FC<ThemeSettingProps> = ({}) => {
-	return <div>Theme</div>;
+	return (
+		<div className='flex flex-col gap-2 rounded-md border border-gray-200 bg-white p-4 md:p-6'>
+			<h2 className='text-lg font-semibold'>Theme</h2>
+			<div className='flex items-center gap-2'>
+				<Switch id='dark-mode-switch' />
+				<Label htmlFor='dark-mode-switch'>Dark mode</Label>
+			</div>
+		</div>
+	);
 };
 
 export default ThemeSetting;
