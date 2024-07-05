f4154196a6944662b4c07022b082bf227df5cac9
Mateusz Mańczak <mateuszmanczak@icloud.com>
2024-07-05 22:46:37 +0200

    add className prop to error and success messages

--- src/components/common/error-message.tsx
+++ src/components/common/error-message.tsx
@@ -1,12 +1,18 @@
+import { cn } from '@/lib/utils';
 import React, { FC } from 'react';
 
 interface Props {
 	children: React.ReactNode;
+	className?: string;
 }
 
-const ErrorMessage: FC<Props> = ({ children }) => {
+const ErrorMessage: FC<Props> = ({ children, className }) => {
 	return (
-		<div className='flex h-8 items-center justify-center rounded-md bg-red-100 px-4 text-red-500'>
+		<div
+			className={cn(
+				'flex h-8 items-center justify-center rounded-md bg-red-100 px-4 text-red-500',
+				className,
+			)}>
 			{children}
 		</div>
 	);
--- src/components/common/success-message.tsx
+++ src/components/common/success-message.tsx
@@ -1,12 +1,18 @@
+import { cn } from '@/lib/utils';
 import React, { FC } from 'react';
 
 interface Props {
 	children: React.ReactNode;
+	className?: string;
 }
 
-const SuccessMessage: FC<Props> = ({ children }) => {
+const SuccessMessage: FC<Props> = ({ children, className }) => {
 	return (
-		<div className='flex h-8 items-center justify-center rounded-md bg-green-100 px-4 text-green-500'>
+		<div
+			className={cn(
+				'flex h-8 items-center justify-center rounded-md bg-green-100 px-4 text-green-500',
+				className,
+			)}>
 			{children}
 		</div>
 	);
