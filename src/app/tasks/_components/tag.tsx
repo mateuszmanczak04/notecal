747bf0b6daaf29f6ea8e7899be9d61ec88e0d3e4
Mateusz Mańczak <mateuszmanczak@icloud.com>
2024-07-05 21:47:40 +0200

    update style of create task page

--- src/app/tasks/_components/tag.tsx
+++ src/app/tasks/_components/tag.tsx
@@ -1,16 +1,18 @@
 import { cn } from '@/lib/utils';
-import { FC } from 'react';
+import { FC, MouseEvent } from 'react';
 
 interface TagProps {
 	text: string;
 	className?: string;
+	onClick?: (event: MouseEvent<HTMLDivElement>) => void;
 }
 
-const Tag: FC<TagProps> = ({ text, className }) => {
+const Tag: FC<TagProps> = ({ text, className, onClick }) => {
 	return (
 		<div
+			onClick={onClick}
 			className={cn(
-				'flex h-6 w-fit cursor-pointer items-center justify-center rounded-md bg-gray-100 px-4',
+				'flex h-6 w-fit cursor-pointer select-none items-center justify-center rounded-md bg-gray-100 px-4 transition hover:bg-gray-200',
 				className,
 			)}>
 			{text}
--- src/app/tasks/create/page.tsx
+++ src/app/tasks/create/page.tsx
@@ -1,13 +1,189 @@
-import CreateTaskForm from '@/app/tasks/_components/create-task-form';
-import { Separator } from '@/components/ui/separator';
+'use client';
+
+import createCourse from '@/app/courses/_actions/create-course';
+import { Button } from '@/components/ui/button';
+import {
+	Form,
+	FormControl,
+	FormDescription,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import CreateCourseSchema from '@/schemas/create-course-schema';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useRouter, useSearchParams } from 'next/navigation';
+import { useState, useTransition } from 'react';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import LoadingSpinner from '@/components/common/loading-spinner';
+import ErrorMessage from '@/components/common/error-message';
+import { useQueryClient } from '@tanstack/react-query';
+import useCourses from '@/app/courses/_hooks/use-courses';
+import CreateTaskSchema from '@/schemas/create-task-schema';
+import { OTHER_COURSE_NAME } from '@/lib/utils';
+import createTask from '../_actions/create-task';
+import queryClient from '@/lib/query-client';
+import Tag from '../_components/tag';
 
 const CreateTaskPage = () => {
+	const {
+		courses,
+		isPending: isCoursesPending,
+		error: coursesError,
+	} = useCourses();
+
+	const searchParams = useSearchParams();
+	const [isPending, startTransition] = useTransition();
+	const [error, setError] = useState('');
+	const form = useForm<z.infer<typeof CreateTaskSchema>>({
+		resolver: zodResolver(CreateTaskSchema),
+		defaultValues: {
+			title: '',
+			description: '',
+			priority: null,
+			dueDate: null,
+			courseId: searchParams.get('courseId') || OTHER_COURSE_NAME,
+		},
+	});
+	const router = useRouter();
+
+	const onSubmit = (values: z.infer<typeof CreateTaskSchema>) => {
+		setError('');
+		startTransition(() => {
+			createTask({
+				...values,
+				// check if courseId should be undefined in request in case it is equal OTHER_COURSE_NAME
+				courseId:
+					values.courseId === OTHER_COURSE_NAME ? undefined : values.courseId,
+			}).then(res => {
+				if (res?.error) {
+					setError(res.error);
+				}
+				queryClient.invalidateQueries({ queryKey: ['tasks'] });
+				// router.push('/tasks');
+				router.back();
+			});
+		});
+	};
+
 	return (
-		<div className='mx-auto max-w-screen-sm p-4'>
-			<h1 className='text-2xl font-semibold'>Create a New Task</h1>
-			<Separator className='mb-6 mt-2' />
-			<CreateTaskForm />
-		</div>
+		<Form {...form}>
+			<form
+				onSubmit={form.handleSubmit(onSubmit)}
+				className='mx-auto mt-4 w-full max-w-[600px] space-y-8'>
+				<h2 className='text-3xl font-bold'>Create a new course</h2>
+				<FormField
+					control={form.control}
+					name='title'
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>
+								Title <span className='text-red-500'>*</span>
+							</FormLabel>
+							<FormControl>
+								<Input
+									placeholder='Do some computer graphics homework'
+									{...field}
+								/>
+							</FormControl>
+							<FormDescription />
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+				<FormField
+					control={form.control}
+					name='description'
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>Description</FormLabel>
+							<FormControl>
+								<Input
+									placeholder='Lorem ipsum something else idk'
+									{...field}
+								/>
+							</FormControl>
+							<FormDescription />
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+				{/* TODO: move existing colors to separate file and make use of them 
+				on the backend */}
+				<FormField
+					control={form.control}
+					name='priority'
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>Priority</FormLabel>
+							<FormControl>
+								<div className='flex gap-2'>
+									<Tag
+										className='flex-1 bg-green-100 text-green-500 hover:bg-green-200'
+										onClick={() => field.onChange('C')}
+										text={'Low'}
+									/>
+									<Tag
+										className='flex-1 bg-yellow-100 text-yellow-500 hover:bg-yellow-200'
+										onClick={() => field.onChange('B')}
+										text={'Medium'}
+									/>
+									<Tag
+										className=' flex-1 bg-red-100 text-red-500 hover:bg-red-200'
+										onClick={() => field.onChange('A')}
+										text={'High'}
+									/>
+								</div>
+							</FormControl>
+						</FormItem>
+					)}
+				/>
+				{/* TODO: make it work */}
+				<FormField
+					control={form.control}
+					name='courseId'
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>Course</FormLabel>
+							<FormControl>
+								<Tag
+									className='w-full flex-1 bg-pink-100 text-pink-500 hover:bg-pink-200'
+									onClick={() => field.onChange('nullid')}
+									text={'Computer Science'}
+								/>
+							</FormControl>
+						</FormItem>
+					)}
+				/>
+				{/* TODO: make it work */}
+				<FormField
+					control={form.control}
+					name='dueDate'
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>Due date</FormLabel>
+							<FormControl>
+								<Tag
+									className='w-full flex-1'
+									onClick={() => field.onChange(new Date())}
+									text={'Computer Science'}
+								/>
+							</FormControl>
+						</FormItem>
+					)}
+				/>
+				<Button type='submit' className='w-full'>
+					Create
+				</Button>
+				<div className='flex w-full justify-center'>
+					{isPending && <LoadingSpinner />}
+				</div>
+				{error && <ErrorMessage>{error}</ErrorMessage>}
+			</form>
+		</Form>
 	);
 };
 
