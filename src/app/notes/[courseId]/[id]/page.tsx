9a97e7b9f4b5be63037bc23bab76fa6ba02ee347
Mateusz Mańczak <mateuszmanczak@icloud.com>
2024-07-06 23:19:04 +0200

    ref(notes): remove unnecessary useEffect inside note page

--- src/app/notes/[courseId]/[id]/page.tsx
+++ src/app/notes/[courseId]/[id]/page.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import DeleteButton from '@/app/notes/_components/delete-button';
 import Content from '@/app/notes/_components/content';
 import Tasks from '@/app/notes/_components/tasks';
@@ -7,26 +5,10 @@ import Teacher from '@/app/notes/_components/teacher';
 import Time from '@/app/notes/_components/time';
 import Title from '@/app/notes/_components/title';
 import SideNotes from '../../_components/side-notes';
-import useNotes from '@/app/notes/_hooks/use-notes';
-import { useQueryClient } from '@tanstack/react-query';
-import { useParams } from 'next/navigation';
-import { useEffect } from 'react';
 import GoBackButton from '@/components/common/go-back-button';
 import { ArrowLeft } from 'lucide-react';
 
 const NotePage = () => {
-	const params = useParams();
-	const queryClient = useQueryClient();
-	const { notes } = useNotes();
-
-	useEffect(() => {
-		const id = params.id;
-		const currentNote = notes?.filter(note => note.id === id)[0];
-		if (!currentNote) {
-			queryClient.invalidateQueries({ queryKey: ['notes'] });
-		}
-	}, [notes, params.id, queryClient]);
-
 	return (
 		<div className='mx-auto flex h-full min-h-80 max-w-[1200px] gap-4'>
 			<div className='flex h-full flex-1 flex-col'>
