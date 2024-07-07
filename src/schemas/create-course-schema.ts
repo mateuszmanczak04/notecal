733473f123527ae14c99cf8d8da48d53a6d20604
Mateusz Mańczak <mateuszmanczak@icloud.com>
2024-07-07 11:44:30 +0200

    chore(courses): update course title and teacher max length in schema

--- src/schemas/create-course-schema.ts
+++ src/schemas/create-course-schema.ts
@@ -1,11 +1,11 @@
 import { z } from 'zod';
 
 const CreateCourseSchema = z.object({
-	name: z.string().min(1, { message: 'Course name is required' }).max(30, {
-		message: 'Maximum length of the course name is 30 characters',
+	name: z.string().min(1, { message: 'Course name is required' }).max(60, {
+		message: 'Maximum length of the course name is 60 characters',
 	}),
-	teacher: z.string().min(1, { message: 'Teacher name is required' }).max(30, {
-		message: 'Maximum length of the teacher name is 30 characters',
+	teacher: z.string().min(1, { message: 'Teacher name is required' }).max(40, {
+		message: 'Maximum length of the teacher name is 40 characters',
 	}),
 	// color: z.string().min(1, { message: 'Course color is required' }),
 });
--- src/schemas/update-course-schema.ts
+++ src/schemas/update-course-schema.ts
@@ -3,13 +3,13 @@ import { z } from 'zod';
 
 const UpdateCourseSchema = z.object({
 	id: z.string().min(1, { message: en.courses.ID_REQUIRED }),
-	newName: z.string().min(1, { message: en.courses.NAME_REQUIRED }).max(30, {
+	newName: z.string().min(1, { message: en.courses.NAME_REQUIRED }).max(60, {
 		message: en.courses.MAX_NAME_LENGTH,
 	}),
 	newTeacher: z
 		.string()
 		.min(1, { message: en.courses.TEACHER_REQUIRED })
-		.max(30, {
+		.max(40, {
 			message: en.courses.MAX_TEACHER_NAME_LENGTH,
 		}),
 });
