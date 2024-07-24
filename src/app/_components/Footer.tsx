9b96b3d67483b478681c5733bbe143f2541dded3
Mateusz Mańczak <mateuszmanczak@icloud.com>
2024-07-24 12:59:01 +0200

    fix(home): footer spacing

--- src/app/_components/Footer.tsx
+++ src/app/_components/Footer.tsx
@@ -3,29 +3,31 @@ import Link from 'next/link';
 const Footer = () => {
 	return (
 		<div className='mt-auto bg-primary-500 p-8 text-white'>
-			<div className='mx-auto grid max-w-screen-sm grid-cols-1 space-y-2 text-center sm:grid-cols-2'>
+			<div className='mx-auto grid max-w-screen-sm grid-cols-1 gap-2 text-center sm:grid-cols-2'>
 				<Link
 					href='https://github.com/mateuszmanczak04/notecal'
-					className='block hover:underline'>
+					className='mt-0 block hover:underline'>
 					Source code (GitHub)
 				</Link>
 				<Link
 					href='https://www.linkedin.com/in/mateusz-manczak/'
-					className='block hover:underline'>
+					className='mt-0 block hover:underline'>
 					Author&apos;s LinkedIn
 				</Link>
 				<Link
 					href='https://www.upwork.com/freelancers/~010d3eb9fdecec067e'
-					className='block hover:underline'>
+					className='mt-0 block hover:underline'>
 					Author&apos;s Upwork
 				</Link>
-				<p className='pointer-events-none block opacity-50'>
+				<Link href='#' className='pointer-events-none block opacity-50 '>
 					Terms & conditions (soon)
-				</p>
-				<p className='pointer-events-none block opacity-50'>
+				</Link>
+				<Link href='#' className='pointer-events-none block opacity-50'>
 					Privacy policy (soon)
-				</p>
-				<p className='pointer-events-none block opacity-50'>Sitemap (soon)</p>
+				</Link>
+				<Link href='#' className='pointer-events-none block opacity-50'>
+					Sitemap (soon)
+				</Link>
 			</div>
 		</div>
 	);
