e3502cc77a2ad18c2e54d084b26422644c3a7db3
Mateusz Mańczak <mateuszmanczak@icloud.com>
2024-08-05 14:40:38 +0200

    fix: remove overflow-x-scroll on layout

--- src/app/_components/main-layout.tsx
+++ src/app/_components/main-layout.tsx
@@ -11,7 +11,7 @@ type MainLayoutProps = {
 const MainLayout = ({ children }: MainLayoutProps) => {
 	return (
 		<Providers>
-			<div className='flex h-screen overflow-y-hidden overflow-x-scroll bg-gray-100 p-4 pl-12 text-gray-900 xl:pl-4'>
+			<div className='flex h-screen overflow-y-hidden bg-gray-100 p-4 pl-12 text-gray-900 xl:pl-4'>
 				<Navigation />
 				<div className='h-full flex-1 overflow-y-scroll rounded-xl bg-white p-4 scrollbar-hide'>
 					{children}
