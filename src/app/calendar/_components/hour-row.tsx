c1cb3ae25815ca76504dade80151bc5b0425e843
Mateusz Mańczak <mateuszmanczak@icloud.com>
2024-08-05 17:10:49 +0200

    style: dont show :00 in calendar hours on mobile

--- src/app/calendar/_components/hour-row.tsx
+++ src/app/calendar/_components/hour-row.tsx
@@ -9,15 +9,12 @@ const HourRow = ({ hour }: { hour: number }) => {
 	return (
 		<div
 			className={cn(
-				'flex flex-col items-center justify-center border-b border-l border-r font-semibold text-neutral-500 sm:flex-row',
+				'flex flex-col items-center justify-center border-b border-l border-r font-semibold text-neutral-500  sm:flex-row',
 				hour === 23 && 'rounded-bl-xl',
 			)}
 			style={{ height: rowHeight + 'px' }}>
-			<span className='text-neutral-800 sm:text-neutral-500'>
-				{hour.toString().padStart(2, '00')}{' '}
-			</span>
-			<span className='hidden sm:inline'>:</span>
-			<span>00</span>
+			<span className=''>{hour.toString().padStart(2, '00')} </span>
+			<span className='hidden sm:inline'>:00</span>
 		</div>
 	);
 };
