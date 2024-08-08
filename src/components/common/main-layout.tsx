70178e5dc36a46d65f599d9eac25de9296420229
Mateusz Mańczak <mateuszmanczak@icloud.com>
2024-08-08 12:46:22 +0200

    feat: dark mode background in main layout

--- src/components/common/main-layout.tsx
+++ src/components/common/main-layout.tsx
@@ -13,7 +13,7 @@ const MainLayout = ({ children }: MainLayoutProps) => {
 		<Providers>
 			<div className='flex h-screen overflow-y-hidden p-4 pl-12 xl:pl-4'>
 				<Navigation />
-				<div className='h-full flex-1 overflow-y-scroll rounded-xl bg-white p-4 scrollbar-hide'>
+				<div className='h-full flex-1 overflow-y-scroll rounded-xl bg-white p-4 scrollbar-hide dark:bg-neutral-800'>
 					{children}
 				</div>
 			</div>
