ed3a4b709529c1d3471fea4f4ef68cf078edc5c8
Mateusz Mańczak <mateuszmanczak@icloud.com>
2024-08-08 12:44:56 +0200

    feat: dark mode in navigation

--- src/components/common/navigation.tsx
+++ src/components/common/navigation.tsx
@@ -12,7 +12,7 @@ import { Skeleton } from '../ui/skeleton';
 
 const Navigation = () => {
 	const pathname = usePathname();
-	const session = useSession();
+	const session = useSession()!;
 
 	const [isOpen, setIsOpen] = useState(false);
 
@@ -24,12 +24,17 @@ const Navigation = () => {
 		setIsOpen(false);
 	};
 
+	// It should never happen because this navigation is not shown
+	// for unauthenticated users
+	if (!session?.data?.user) return;
+
 	return (
 		<>
+			{/* Open button */}
 			<Button
 				variant='secondary'
 				className={cn(
-					'transiton fixed left-0 top-0 z-10 h-full w-12 translate-x-0 rounded-none xl:hidden',
+					'transiton fixed left-0 top-0 z-10 h-full w-12 translate-x-0 rounded-none xl:hidden dark:bg-neutral-900 dark:text-neutral-100',
 					isOpen && '-translate-x-32',
 				)}
 				onClick={handleOpen}>
@@ -37,54 +42,53 @@ const Navigation = () => {
 			</Button>
 			<div
 				className={cn(
-					'fixed left-0 top-0 z-40 mr-4 flex h-full w-screen shrink-0 translate-x-0 flex-col gap-8 bg-neutral-100 p-8 pl-20 shadow-xl transition sm:w-96 xl:static xl:max-w-80 xl:p-4 xl:shadow-none',
+					'fixed left-0 top-0 z-40 mr-4 flex h-full w-screen shrink-0 translate-x-0 flex-col gap-8 rounded-xl bg-neutral-100 p-8 pl-20 shadow-lg transition sm:w-96 xl:static xl:max-w-80 xl:p-4 xl:shadow-none dark:bg-neutral-800',
 					!isOpen && '-translate-x-full xl:translate-x-0',
 				)}>
+				{/* Close button */}
 				<Button
 					variant='secondary'
-					className='transiton fixed left-0 top-0 z-10 h-full w-12 translate-x-0 rounded-none bg-neutral-200 xl:hidden'
+					className='transiton fixed left-0 top-0 z-10 h-full w-12 translate-x-0 rounded-none bg-neutral-200 xl:hidden dark:bg-neutral-900 dark:text-neutral-100'
 					onClick={handleClose}>
 					<X />
 				</Button>
-				{/* Account and settings: */}
-				{session?.data?.user ? (
-					<Link
-						href='/settings'
-						onClick={handleClose}
-						className='flex items-center justify-between gap-3 rounded-xl bg-white px-4 py-2'>
-						{/* <Image
+
+				{/* Account and settings */}
+				<Link
+					href='/settings'
+					onClick={handleClose}
+					className='flex items-center justify-between gap-3 rounded-xl bg-white px-4 py-2 dark:bg-neutral-700'>
+					{/* <Image
 							src='/avatar.jpg'
 							width={32}
 							height={32}
 							alt='profile picture'
 							className='h-8 w-8 overflow-hidden rounded-full object-cover'
 						/> */}
-						<User className='h-8 w-8 shrink-0 rounded-full bg-neutral-100 p-1' />
-						<div className='overflow-hidden'>
-							<p className='truncate text-sm font-bold'>
-								{session.data.user.email?.split('@')[0]}
-							</p>
-							<p className='truncate text-sm text-neutral-500'>
-								{session.data.user.email}
-							</p>
-						</div>
+					<User className='h-8 w-8 shrink-0 rounded-full bg-neutral-100 p-1 dark:bg-neutral-800' />
+					<div className='overflow-hidden'>
+						<p className='truncate text-sm font-medium'>
+							{session.data.user.email?.split('@')[0]}
+						</p>
+						<p className='truncate text-sm opacity-50'>
+							{session.data.user.email}
+						</p>
+					</div>
+					<Cog className='shrin h-4 w-4' />
+				</Link>
 
-						<Cog className='shrin h-4 w-4' />
-					</Link>
-				) : (
-					<Skeleton className='h-12 w-full bg-white' />
-				)}
-				{/* Main menu: */}
+				{/* Main menu */}
 				<div>
-					<p className='text-sm font-semibold uppercase text-neutral-500'>
+					<p className='text-sm font-semibold uppercase opacity-50'>
 						MAIN MENU
 					</p>
 					<div className='mt-2'>
 						<Link
 							href='/calendar'
 							className={cn(
-								'flex items-center gap-2 rounded-sm p-2 font-semibold',
-								pathname.includes('/calendar') && 'bg-white',
+								'flex items-center gap-2 rounded-xl px-3 py-2 font-semibold',
+								pathname.includes('/calendar') &&
+									'bg-white dark:bg-neutral-700',
 							)}
 							onClick={handleClose}>
 							<Calendar className='h-4 w-4' /> Calendar
@@ -92,8 +96,8 @@ const Navigation = () => {
 						<Link
 							href='/tasks'
 							className={cn(
-								'mt-2 flex items-center gap-2 rounded-sm p-2 font-semibold',
-								pathname.includes('/tasks') && 'bg-white',
+								'mt-2 flex items-center gap-2 rounded-xl px-3 py-2 font-semibold',
+								pathname.includes('/tasks') && 'bg-white dark:bg-neutral-700',
 							)}
 							onClick={handleClose}>
 							<Check className='h-4 w-4' /> Tasks
@@ -101,8 +105,8 @@ const Navigation = () => {
 						<Link
 							href='/courses'
 							className={cn(
-								'mt-2 flex items-center gap-2 rounded-sm p-2 font-semibold',
-								pathname.includes('/courses') && 'bg-white',
+								'mt-2 flex items-center gap-2 rounded-xl px-3 py-2 font-semibold',
+								pathname.includes('/courses') && 'bg-white dark:bg-neutral-700',
 							)}
 							onClick={handleClose}>
 							<List className='h-4 w-4' /> Courses
