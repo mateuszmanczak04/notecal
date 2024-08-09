beb93e95ec57df462ebd64ccfc2f94b910cf3785
Mateusz Mańczak <mateuszmanczak@icloud.com>
2024-08-09 10:19:33 +0200

    style(tasks): update height of create-priority elements

--- src/app/tasks/_components/create-priority.tsx
+++ src/app/tasks/_components/create-priority.tsx
@@ -23,7 +23,7 @@ const CreatePriority = ({ field }: Props) => {
 		<div className='grid grid-cols-3 gap-2 transition'>
 			<Tag
 				className={cn(
-					'bg-green-100 text-green-500 transition-all duration-500 hover:bg-green-200 dark:bg-green-800 dark:text-green-100 ',
+					'h-9 bg-green-100 text-green-500 transition-all duration-500 hover:bg-green-200 dark:bg-green-800 dark:text-green-100 ',
 					field.value === 'C' &&
 						'bg-green-500 text-white hover:bg-green-400 dark:bg-green-500 dark:text-white',
 					(field.value === 'A' || field.value === 'B') && 'opacity-50',
@@ -39,7 +39,7 @@ const CreatePriority = ({ field }: Props) => {
 			/>
 			<Tag
 				className={cn(
-					'bg-yellow-100 text-yellow-500 transition-all duration-500 hover:bg-yellow-200 dark:bg-yellow-700 dark:text-yellow-100',
+					'h-9 bg-yellow-100 text-yellow-500 transition-all duration-500 hover:bg-yellow-200 dark:bg-yellow-700 dark:text-yellow-100',
 					field.value === 'B' &&
 						'bg-yellow-500 text-white hover:bg-yellow-400 dark:bg-yellow-500 dark:text-white',
 					(field.value === 'A' || field.value === 'C') && 'opacity-50',
@@ -55,7 +55,7 @@ const CreatePriority = ({ field }: Props) => {
 			/>
 			<Tag
 				className={cn(
-					'bg-red-100 text-red-500 transition-all duration-500 hover:bg-red-200 dark:bg-red-800 dark:text-red-100',
+					'h-9 bg-red-100 text-red-500 transition-all duration-500 hover:bg-red-200 dark:bg-red-800 dark:text-red-100',
 					field.value === 'A' &&
 						'bg-red-500 text-white hover:bg-red-400 dark:bg-red-500 dark:text-white',
 					(field.value === 'C' || field.value === 'B') && 'opacity-50',
