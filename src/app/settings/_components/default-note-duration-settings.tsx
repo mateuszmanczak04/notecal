0fb3eb63d647efcc495ea844b471ad59f015d28f
Mateusz Mańczak <mateuszmanczak@icloud.com>
2024-08-21 09:45:27 +0200

    feat: replace default note duration setting with dropdown

--- src/app/settings/_components/default-note-duration-settings.tsx
+++ src/app/settings/_components/default-note-duration-settings.tsx
@@ -1,19 +1,9 @@
 'use client';
 
-import { Button } from '@/components/ui/button';
-import {
-	Form,
-	FormControl,
-	FormField,
-	FormItem,
-	FormMessage,
-} from '@/components/ui/form';
-import { Input } from '@/components/ui/input';
-import UpdateDefaultNoteDurationSchema from '@/schemas/update-default-note-duration-schema';
-import { zodResolver } from '@hookform/resolvers/zod';
+import DropdownMenu, {
+	DropdownMenuValueType,
+} from '@/components/common/dropdown-menu';
 import { FC } from 'react';
-import { useForm } from 'react-hook-form';
-import { z } from 'zod';
 import useSettings from '../_hooks/use-settings';
 
 interface DefaultNoteDurationSettingProps {
@@ -23,50 +13,59 @@ interface DefaultNoteDurationSettingProps {
 const DefaultNoteDurationSetting: FC<DefaultNoteDurationSettingProps> = ({
 	initialDefaultNoteDuration,
 }) => {
-	const form = useForm<z.infer<typeof UpdateDefaultNoteDurationSchema>>({
-		resolver: zodResolver(UpdateDefaultNoteDurationSchema),
-		defaultValues: {
-			defaultNoteDuration: initialDefaultNoteDuration,
-		},
-	});
 	const { update: updateSettings } = useSettings();
 
-	const handleSubmit = (
-		values: z.infer<typeof UpdateDefaultNoteDurationSchema>,
-	) => {
-		updateSettings({
-			defaultNoteDuration: values.defaultNoteDuration,
-		});
+	const options = [
+		{
+			value: 15,
+			label: '15 min',
+		},
+		{
+			value: 30,
+			label: '30 min',
+		},
+		{
+			value: 45,
+			label: '45 min',
+		},
+		{
+			value: 60,
+			label: '60 min',
+		},
+		{
+			value: 90,
+			label: '90 min',
+		},
+		{
+			value: 120,
+			label: '120 min',
+		},
+		{
+			value: 180,
+			label: '180 min',
+		},
+	];
+
+	const currentOption = {
+		value: initialDefaultNoteDuration,
+		label: initialDefaultNoteDuration.toString() + ' min',
+	};
+
+	const handleChange = (value: DropdownMenuValueType) => {
+		updateSettings({ defaultNoteDuration: value as number });
 	};
 
 	return (
-		<Form {...form}>
-			<form onSubmit={form.handleSubmit(handleSubmit)}>
-				<h2 className='mb-2 text-lg font-semibold'>
-					Default note duration (in minutes)
-				</h2>
-				<div className='grid gap-2 sm:grid-cols-2'>
-					<FormField
-						control={form.control}
-						name='defaultNoteDuration'
-						render={({ field }) => (
-							<FormItem>
-								<FormControl>
-									<Input
-										placeholder='60'
-										type='number'
-										{...field}
-										className='w-full'
-									/>
-								</FormControl>
-								<FormMessage />
-							</FormItem>
-						)}
-					/>
-					<Button type='submit'>Save</Button>
-				</div>
-			</form>
-		</Form>
+		<div>
+			<h2 className='mb-2 text-lg font-semibold'>
+				Amount of days in week view
+			</h2>
+			<DropdownMenu
+				options={options}
+				currentOption={currentOption}
+				onChange={handleChange}
+			/>
+		</div>
 	);
 };
 
