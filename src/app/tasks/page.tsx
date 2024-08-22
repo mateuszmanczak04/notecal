d911cd3bf4e8d98cd2ab25d5769f1231bd73c161
Mateusz Mańczak <mateuszmanczak@icloud.com>
2024-08-22 13:10:46 +0200

    style: improve tasks loading screen style

--- src/app/tasks/page.tsx
+++ src/app/tasks/page.tsx
@@ -1,11 +1,46 @@
-import Tasks from './_components/tasks';
+'use client';
 
-const page = () => {
+import ErrorMessage from '@/components/common/error-message';
+import LoadingSpinner from '@/components/common/loading-spinner';
+import useCourses from '../courses/_hooks/use-courses';
+import Task from './_components/task';
+import useTasks from './_hooks/use-tasks';
+
+const TasksPage = () => {
+	const { tasks, isPending, error } = useTasks();
+	const { isPending: isPendingCourses } = useCourses();
+
+	if (isPending || isPendingCourses) {
+		return (
+			<div className='grid h-full w-full place-content-center '>
+				<div className='flex flex-col gap-4'>
+					<LoadingSpinner />
+					<p className='animate-pulse'>
+						Give us a second, we are loading your notes and courses
+						😅
+					</p>
+				</div>
+			</div>
+		);
+	}
+
 	return (
-		<div className='mx-auto flex max-w-[800px] flex-col gap-8'>
-			<Tasks />
+		<div className='mx-auto max-w-[800px] space-y-8'>
+			{error && <ErrorMessage>{error.message}</ErrorMessage>}
+
+			{/* No tasks */}
+			{(!tasks || tasks.length === 0) && (
+				<p className='text-lg text-neutral-500'>
+					You don&apos;t have any tasks yet.
+				</p>
+			)}
+
+			{/* At least 1 task */}
+			{tasks &&
+				tasks.length > 0 &&
+				tasks.map(task => <Task key={task.id} task={task} />)}
 		</div>
 	);
 };
 
-export default page;
+export default TasksPage;
