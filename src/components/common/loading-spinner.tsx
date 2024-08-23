497c738bb0bc4d644cf1b36d519ec98411f2e039
Mateusz Mańczak <mateuszmanczak@icloud.com>
2024-08-23 12:14:08 +0200

    style: replace LoadingSpinner component with just an animated div
    ref: uninstall react-spinners

--- src/components/common/loading-spinner.tsx
+++ src/components/common/loading-spinner.tsx
@@ -1,8 +1,15 @@
 import { cn } from '@/lib/utils';
-import { ScaleLoader } from 'react-spinners';
 
 const LoadingSpinner = ({ className }: { className?: string }) => {
-	return <ScaleLoader className={cn('mx-auto', className)} />;
+	return (
+		<div
+			className={cn(
+				'mx-auto inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] text-neutral-800 motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-neutral-100',
+				className,
+			)}
+			role='status'
+			aria-label='loading spinner'></div>
+	);
 };
 
 export default LoadingSpinner;
