2e416734937518d67911415d674f3161ebb9dc7b
Mateusz Mańczak <mateuszmanczak@icloud.com>
2024-08-23 10:49:23 +0200

    feat: add edit course link in /note page

--- src/app/notes/[courseId]/[id]/page.tsx
+++ src/app/notes/[courseId]/[id]/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import Content from '@/app/notes/_components/content';
 import DeleteButton from '@/app/notes/_components/delete-button';
 import Tasks from '@/app/notes/_components/tasks';
@@ -5,17 +7,30 @@ import Teacher from '@/app/notes/_components/teacher';
 import Time from '@/app/notes/_components/time';
 import Title from '@/app/notes/_components/title';
 import GoBackButton from '@/components/common/go-back-button';
-import { ArrowLeft } from 'lucide-react';
+import { Button } from '@/components/ui/button';
+import { ArrowLeft, Pencil } from 'lucide-react';
+import Link from 'next/link';
 import SideNotes from '../../_components/side-notes';
+import { useNoteContext } from '../../_context/note-context';
 
 const NotePage = () => {
+	const { course } = useNoteContext();
+
 	return (
 		<div className='mx-auto flex h-full min-h-80 max-w-[1200px] gap-4'>
 			<div className='flex h-full flex-1 flex-col'>
-				<GoBackButton variant='secondary' className='w-fit'>
-					<ArrowLeft />
-					Go back
-				</GoBackButton>
+				<div className='flex gap-2'>
+					<GoBackButton variant='secondary' className='w-fit'>
+						<ArrowLeft />
+						Go back
+					</GoBackButton>
+					<Button asChild variant='secondary' className='gap-1'>
+						<Link href={`/courses/edit?id=${course.id}`}>
+							<Pencil />
+							Edit
+						</Link>
+					</Button>
+				</div>
 				<Title />
 				<Time />
 				<Content />
