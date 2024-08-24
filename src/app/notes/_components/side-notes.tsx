ac5bdc4a796de72942f403e28f373619f956484f
Mateusz Mańczak <mateuszmanczak@icloud.com>
2024-08-24 14:02:34 +0200

    fix: dark mode in side notes

--- src/app/notes/_components/side-notes.tsx
+++ src/app/notes/_components/side-notes.tsx
@@ -22,16 +22,17 @@ const SideNotes = () => {
 						aria-label={`link to note with date ${format(note.startTime, 'yyyy-MM-dd')}`}
 						title={`link to note with date ${format(note.startTime, 'yyyy-MM-dd')}`}
 						className={cn(
-							'flex h-9 items-center gap-2 border-b border-l border-r px-3 hover:bg-neutral-100',
+							'flex h-9 items-center gap-2 border-b border-l border-r px-3 hover:bg-neutral-100 dark:border-neutral-700 dark:hover:bg-neutral-700',
 							index === 0 && 'rounded-t-xl border-t',
 							index === notes.length - 1 &&
 								'border-b-transparent',
-							note.id === params.id && 'bg-neutral-200',
+							note.id === params.id &&
+								'bg-neutral-200 dark:bg-neutral-600',
 						)}>
 						<span className='shrink-0 text-sm'>
 							{format(note.startTime, 'yyyy-MM-dd')}
 						</span>
-						<span className='line-clamp-1 text-sm text-neutral-600'>
+						<span className='line-clamp-1 text-sm text-neutral-600 dark:text-neutral-300'>
 							{note.content}
 						</span>
 					</Link>
