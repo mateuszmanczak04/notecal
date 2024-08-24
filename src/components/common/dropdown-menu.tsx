d38893cc70c55a0123e4c1222b5b0553c3b7b79a
Mateusz Mańczak <mateuszmanczak@icloud.com>
2024-08-24 18:54:23 +0200

    fix: remove unnecessary space

--- src/components/common/dropdown-menu.tsx
+++ src/components/common/dropdown-menu.tsx
@@ -119,7 +119,7 @@ export const DropdownMenuTrigger = ({
 				isOpen ? 'rounded-t-xl dark:border-neutral-500' : 'rounded-xl',
 				className,
 			)}>
-			{children}{' '}
+			{children}
 			{showChevron && <ChevronDown className='h-5 w-5 shrink-0' />}
 		</Tag>
 	);
