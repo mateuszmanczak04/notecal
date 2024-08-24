43341758727b6d0bcb74d4f69745ca4f68291466
Mateusz Mańczak <mateuszmanczak@icloud.com>
2024-08-24 17:42:00 +0200

    ref(settings): simplify code of default note duration

--- src/app/settings/_components/default-note-duration-settings.tsx
+++ src/app/settings/_components/default-note-duration-settings.tsx
@@ -1,58 +1,22 @@
 'use client';
 
-import DropdownMenu, {
-	DropdownMenuValueType,
+import {
+	DropdownMenu,
+	DropdownMenuItem,
+	DropdownMenuList,
+	DropdownMenuTrigger,
 } from '@/components/common/dropdown-menu';
-import { FC } from 'react';
 import useSettings from '../_hooks/use-settings';
 
-interface DefaultNoteDurationSettingProps {
+type Props = {
 	initialDefaultNoteDuration: number;
-}
+};
 
-const DefaultNoteDurationSetting: FC<DefaultNoteDurationSettingProps> = ({
-	initialDefaultNoteDuration,
-}) => {
+const DefaultNoteDurationSetting = ({ initialDefaultNoteDuration }: Props) => {
 	const { update: updateSettings } = useSettings();
 
-	const options = [
-		{
-			value: 15,
-			label: '15 min',
-		},
-		{
-			value: 30,
-			label: '30 min',
-		},
-		{
-			value: 45,
-			label: '45 min',
-		},
-		{
-			value: 60,
-			label: '60 min',
-		},
-		{
-			value: 90,
-			label: '90 min',
-		},
-		{
-			value: 120,
-			label: '120 min',
-		},
-		{
-			value: 180,
-			label: '180 min',
-		},
-	];
-
-	const currentOption = {
-		value: initialDefaultNoteDuration,
-		label: initialDefaultNoteDuration.toString() + ' min',
-	};
-
-	const handleChange = (value: DropdownMenuValueType) => {
-		updateSettings({ defaultNoteDuration: value as number });
+	const handleSelect = (defaultNoteDuration: number) => {
+		updateSettings({ defaultNoteDuration });
 	};
 
 	return (
@@ -60,11 +24,21 @@ const DefaultNoteDurationSetting: FC<DefaultNoteDurationSettingProps> = ({
 			<h2 className='mb-2 text-lg font-semibold'>
 				Default new note duration
 			</h2>
-			<DropdownMenu
-				options={options}
-				currentOption={currentOption}
-				onChange={handleChange}
-			/>
+			<DropdownMenu>
+				<DropdownMenuTrigger>
+					{initialDefaultNoteDuration} min
+				</DropdownMenuTrigger>
+				<DropdownMenuList>
+					{[15, 30, 45, 60, 90, 120, 180].map(minutes => (
+						<DropdownMenuItem
+							onSelect={handleSelect}
+							key={minutes}
+							value={minutes}>
+							{minutes} min
+						</DropdownMenuItem>
+					))}
+				</DropdownMenuList>
+			</DropdownMenu>
 		</div>
 	);
 };
