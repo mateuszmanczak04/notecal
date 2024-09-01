67d28b87a2d884dcbf8011ff5b3cb4f4a8ea8ea8
Mateusz Mańczak <mateuszmanczak@icloud.com>
2024-09-01 15:33:54 +0200

    feat: create TasksHistoryContextProvider draft

---
+++ src/app/tasks/_context/tasks-history-context.tsx
@@ -0,0 +1,51 @@
+import { OrderTasksEnum } from '@prisma/client';
+import React, { createContext } from 'react';
+
+type SortParams = {
+	type: 'sort';
+	old: OrderTasksEnum;
+	new: OrderTasksEnum;
+};
+
+type UpdateParams<T, V> = {
+	type: 'update';
+	taskId: string;
+	property: T;
+	oldValue: V;
+	newValue: V;
+};
+
+type ChangeParams =
+	| SortParams
+	| UpdateParams<'completed', boolean>
+	| UpdateParams<'title' | 'courseId' | 'description', string>
+	| UpdateParams<'dueDate', Date | null>
+	| UpdateParams<'priority', 'A' | 'B' | 'C' | null>;
+
+type Props = {
+	undo: () => void;
+	redo: () => void;
+	change: (params: ChangeParams) => void;
+};
+
+const TasksHistoryContext = createContext({} as Props);
+
+const TasksHistoryContextProvider = ({
+	children,
+}: {
+	children: React.ReactNode;
+}) => {
+	const change = (params: ChangeParams) => {};
+
+	const undo = () => {};
+
+	const redo = () => {};
+
+	return (
+		<TasksHistoryContext.Provider value={{ undo, redo, change }}>
+			{children}
+		</TasksHistoryContext.Provider>
+	);
+};
+
+export default TasksHistoryContextProvider;
