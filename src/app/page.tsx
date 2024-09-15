00594a5b0deedd989cb3869d658c43e81dd08792
Mateusz Mańczak <mateuszmanczak@icloud.com>
2024-09-15 16:22:31 +0200

    Remove preloading user settings in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,13 @@
-import queryClient from '@/lib/query-client';
 import Footer from '../components/common/footer';
 import Hero from '../components/common/hero';
-import getSettings from './settings/_actions/get-settings';
 
-export default async function Home() {
-	await queryClient.prefetchQuery({
-		queryKey: ['settings'],
-		queryFn: getSettings,
-	});
-
+const page = () => {
 	return (
 		<div className='flex flex-col'>
 			<Hero />
 			<Footer />
 		</div>
 	);
-}
+};
+
+export default page;
