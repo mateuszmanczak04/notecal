37b2da990f92d53b7cdb0d426579a5776684de02
Mateusz Mańczak <mateuszmanczak@icloud.com>
2024-11-04 13:39:48 +0100

    Update error page metadata

--- src/app/auth/error/page.tsx
+++ src/app/auth/error/page.tsx
@@ -1,4 +1,12 @@
 import ErrorMessage from '@/components/common/error-message';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+	title: 'An error occurred',
+	robots: {
+		index: false,
+	},
+};
 
 const AuthErrorPage = () => {
 	return <ErrorMessage>Auth error occurred</ErrorMessage>;
