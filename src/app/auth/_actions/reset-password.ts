0284b8c4ca3403131ef9064550dc2773c4ebc931
Mateusz Mańczak <mateuszmanczak@icloud.com>
2024-12-22 13:40:01 +0100

    Move resetPassword function to separate file

---
+++ src/app/auth/_actions/reset-password.ts
@@ -0,0 +1,73 @@
+'use server';
+
+import db from '@/lib/db';
+import { generateToken } from '@/lib/jwt';
+import bcryptjs from 'bcryptjs';
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
+
+/**
+ * An action invoked after form submission in /auth/reset-password. It updates user's password and logs they in. Only available after requesting recovery email when user has forgotten their password.
+ */
+const resetPassword = async (formData: FormData) => {
+	const password = formData.get('password')?.toString();
+	const email = formData.get('email')?.toString();
+	const token = formData.get('token')?.toString();
+
+	// Validate the password
+	if (!email || email.length === 0 || !password || password.length === 0) return;
+
+	// Token is missing
+	if (!token) {
+		redirect('/auth/reset-password/invalid-token');
+	}
+
+	const resetToken = await db.resetPasswordToken.findFirst({
+		where: { token },
+	});
+
+	// Token doesn't exist in the db
+	if (!resetToken) {
+		redirect('/auth/reset-password/invalid-token');
+	}
+
+	// Token is not associated with the email
+	if (resetToken.email !== email) {
+		redirect('/auth/reset-password/forbidden');
+	}
+
+	// Check if user with current email exists
+	const user = await db.user.findFirst({
+		where: { email: resetToken.email },
+	});
+	if (!user) {
+		redirect('/auth/reset-password/forbidden');
+	}
+
+	// Update user's password
+	const hashedPassword = await bcryptjs.hash(password, 10);
+	await db.user.update({
+		where: { email: resetToken.email },
+		data: { password: hashedPassword },
+	});
+
+	// Delete token
+	await db.resetPasswordToken.deleteMany({
+		where: { email: resetToken.email },
+	});
+
+	// Generate JWT
+	const jwtToken = await generateToken({ id: user.id });
+
+	// Automatically sign user in
+	const cookieStore = await cookies();
+	cookieStore.set('authToken', jwtToken, {
+		httpOnly: true,
+		secure: true,
+		sameSite: 'strict',
+	});
+
+	redirect('/calendar');
+};
+
+export default resetPassword;
--- src/app/auth/reset-password/page.tsx
+++ src/app/auth/reset-password/page.tsx
@@ -1,11 +1,9 @@
 import FormLoadingSpinner from '@/components/common/form-loading-spinner';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
-import db from '@/lib/db';
-import brcyptjs from 'bcryptjs';
 import { Metadata } from 'next';
 import { redirect } from 'next/navigation';
-import login from '../_actions/login';
+import resetPassword from '../_actions/reset-password';
 
 export const metadata: Metadata = {
 	title: 'Reset your password',
@@ -27,56 +25,6 @@ const page = async (props: Props) => {
 		redirect('/auth/reset-password/invalid-url');
 	}
 
-	/**
-	 * An action invoked after form submission. It updates user's password and logs they in.
-	 */
-	const formAction = async (formData: FormData) => {
-		'use server';
-		const password = formData.get('password')?.toString();
-
-		// Validate the password
-		if (!password || password.length === 0) return;
-
-		const resetToken = await db.resetPasswordToken.findFirst({
-			where: { token },
-		});
-
-		// If token doesn't exist in db, show error message to user
-		if (!resetToken) {
-			redirect('/auth/reset-password/invalid-token');
-		}
-
-		if (resetToken.email !== email) {
-			redirect('/auth/reset-password/forbidden');
-		}
-
-		// Check if user with current email exists
-		const user = await db.user.findFirst({
-			where: { email: resetToken.email },
-		});
-		if (!user) {
-			redirect('/auth/reset-password/forbidden');
-		}
-
-		// Update user's password
-		const hashedPassword = await brcyptjs.hash(password, 10);
-		await db.user.update({
-			where: { email: resetToken.email },
-			data: { password: hashedPassword },
-		});
-
-		// Delete token
-		await db.resetPasswordToken.deleteMany({
-			where: { email: resetToken.email },
-		});
-
-		// Automatically log user in
-		const fd = new FormData();
-		fd.set('email', email);
-		fd.set('password', password);
-		await login(fd);
-	};
-
 	return (
 		<main className='mx-auto  max-w-lg px-4'>
 			<h1 className='px-4 text-3xl font-bold'>Reset Your Password</h1>
@@ -87,11 +35,14 @@ const page = async (props: Props) => {
 
 			<form
 				className='mt-4 rounded-xl border border-neutral-200 p-4 dark:border-transparent dark:bg-neutral-800'
-				action={formAction}>
+				action={resetPassword}>
 				<label htmlFor='password' className='ml-2 block font-medium'>
 					New Password
 				</label>
 				<Input id='password' name='password' type='password' className='mt-1' required placeholder='*******' />
+
+				<input type='hidden' name='email' value={email} />
+				<input type='hidden' name='token' value={token} />
 
 				<Button type='submit' className='mt-4 w-full'>
 					<FormLoadingSpinner className='h-5 w-5' />
