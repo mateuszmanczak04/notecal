c944c44b7b06cb9f6778a37344dbdf5d6cc02ba1
Mateusz Mańczak <mateuszmanczak@icloud.com>
2024-12-22 19:03:00 +0100

    Fix courses list page

--- src/app/courses/_components/courses.tsx
+++ src/app/courses/_components/courses.tsx
@@ -1,60 +1,43 @@
 'use client';
 
-import ErrorMessage from '@/components/common/error-message';
-import LoadingSpinner from '@/components/common/loading-spinner';
 import { Course as T_Course } from '@prisma/client';
 import { Plus } from 'lucide-react';
-import { Metadata } from 'next';
 import Link from 'next/link';
+import { use } from 'react';
 import Course from './course';
 
-export const metadata: Metadata = {
-	title: 'Courses',
-	robots: {
-		index: false,
-	},
+type Props = {
+	coursesPromise: Promise<T_Course[]>;
 };
 
-const Courses = () => {
-	const courses = [] as T_Course[];
-	const isPending = false;
-	const error = { message: '' };
+const Courses = ({ coursesPromise }: Props) => {
+	const courses = use(coursesPromise);
+
+	if (!courses) {
+		return <p className='text-center text-lg text-neutral-500 sm:ml-8'>You don&apos;t have any courses yet.</p>;
+	}
 
 	return (
-		<div>
-			{error && <ErrorMessage>{error.message}</ErrorMessage>}
+		<>
+			<div className='grid gap-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5'>
+				{courses?.map(course => (
+					<Course
+						color={course.color}
+						key={course.id}
+						id={course.id}
+						name={course.name}
+						teacher={course.teacher}
+					/>
+				))}
 
-			{isPending && (
-				<div className='grid place-content-center'>
-					<LoadingSpinner />
-				</div>
-			)}
-
-			{(!courses || courses.length === 0) && !isPending && (
-				<p className='text-center text-lg text-neutral-500 sm:ml-8'>You don&apos;t have any courses yet.</p>
-			)}
-
-			{!isPending && (
-				<div className='grid gap-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5'>
-					{courses?.map(course => (
-						<Course
-							color={course.color}
-							key={course.id}
-							id={course.id}
-							name={course.name}
-							teacher={course.teacher}
-						/>
-					))}
-
-					<Link
-						prefetch
-						href='/courses/create'
-						className='grid cursor-pointer place-content-center gap-2 rounded-xl bg-neutral-100 p-4 transition hover:bg-neutral-200 dark:bg-neutral-700 dark:text-neutral-100 dark:hover:bg-neutral-600'>
-						<Plus className='h-10 w-10' />
-					</Link>
-				</div>
-			)}
-		</div>
+				<Link
+					prefetch
+					href='/courses/create'
+					className='grid cursor-pointer place-content-center gap-2 rounded-xl bg-neutral-100 p-4 transition hover:bg-neutral-200 dark:bg-neutral-700 dark:text-neutral-100 dark:hover:bg-neutral-600'>
+					<Plus className='h-10 w-10' />
+				</Link>
+			</div>
+		</>
 	);
 };
 
--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -1,4 +1,8 @@
+import LoadingSpinner from '@/components/common/loading-spinner';
+import { getAuthStatus } from '@/lib/auth';
+import db from '@/lib/db';
 import { Metadata } from 'next';
+import { Suspense } from 'react';
 import Courses from './_components/courses';
 
 export const metadata: Metadata = {
@@ -8,6 +12,25 @@ export const metadata: Metadata = {
 	},
 };
 
-const page = () => <Courses />;
+const page = async () => {
+	const { user } = (await getAuthStatus()) as { user: { id: string } };
+
+	const coursesPromise = db.course.findMany({
+		where: {
+			userId: user.id,
+		},
+		orderBy: {
+			name: 'asc',
+		},
+	});
+
+	return (
+		<main>
+			<Suspense fallback={<LoadingSpinner />}>
+				<Courses coursesPromise={coursesPromise} />
+			</Suspense>
+		</main>
+	);
+};
 
 export default page;
