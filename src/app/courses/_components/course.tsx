1ce668217339ebd7c0eecccc43fbed853721f245
Mateusz Mańczak <mateuszmanczak@icloud.com>
2024-12-22 19:35:15 +0100

    Simplify course item

--- src/app/courses/_components/course.tsx
+++ src/app/courses/_components/course.tsx
@@ -1,57 +1,25 @@
-import useNotes from '@/app/notes/_hooks/use-notes';
 import { ChevronRight } from 'lucide-react';
-import { useRouter } from 'next/navigation';
-import { FC } from 'react';
-import { uid } from 'uid';
+import Link from 'next/link';
 
-interface CourseProps {
+type Props = {
 	name: string;
 	id: string;
 	teacher: string;
 	color: string;
-}
+};
 
-const Course: FC<CourseProps> = ({ name, teacher, id, color }) => {
-	const { notes, isPending } = useNotes();
-	const filteredNotes = notes?.filter(note => note.courseId === id);
-	const settings = {};
-	const { add: addNewNote } = useNotes();
-	const router = useRouter();
-
-	const handleClick = () => {
-		if (!settings || !notes) return;
-
-		const courseNotes = notes.filter(n => n.courseId === id);
-
-		if (courseNotes.length > 0) {
-			router.push(`/notes/${courseNotes.at(-1)?.id}`);
-		} else {
-			// Create first course note
-			const startTime = new Date();
-			const endTime = new Date(startTime.getTime() + settings.defaultNoteDuration * 60 * 1000);
-			const newNoteId = uid();
-			addNewNote({
-				id: newNoteId,
-				courseId: id,
-				content: '',
-				startTime,
-				endTime,
-			});
-			router.push(`/notes/${id}/${newNoteId}`);
-		}
-	};
-
+const Course = ({ name, teacher, id, color }: Props) => {
 	return (
-		<div
+		<Link
+			href={`/notes/${id}`}
 			className='flex cursor-pointer items-center justify-between rounded-xl bg-neutral-50 p-4 text-white transition hover:opacity-90'
-			style={{ background: color }}
-			onClick={handleClick}>
+			style={{ background: color }}>
 			<div>
 				<p className='line-clamp-1 rounded-xl text-xl font-medium'>{name}</p>
 				<p className='mt-1 line-clamp-1 opacity-75'>{teacher}</p>
 			</div>
 			<ChevronRight className='h-10 w-10 shrink-0' />
-		</div>
+		</Link>
 	);
 };
 
