bf55c656058829634b4af3827b36ff296cdf668c
Mateusz Mańczak <mateuszmanczak@icloud.com>
2024-12-22 18:08:29 +0100

    Fix adding tasks

--- src/app/tasks/_actions/create-task.ts
+++ src/app/tasks/_actions/create-task.ts
@@ -1,55 +1,33 @@
 'use server';
 
+import { getAuthStatus } from '@/lib/auth';
+import db from '@/lib/db';
 import { en } from '@/lib/dictionary';
-import CreateTaskSchema from '@/schemas/create-task-schema';
-import { z } from 'zod';
 
-const createTask = async (values: z.infer<typeof CreateTaskSchema>) => {
-	const validatedFields = CreateTaskSchema.safeParse(values);
+const createTask = async (_prevState: any, formData: FormData) => {
+	const title = formData.get('title')?.toString();
 
-	if (!validatedFields.success) {
-		return { error: en.INVALID_DATA };
+	if (!title) {
+		return { error: 'Task title is required' };
 	}
 
-	const { id, courseId, title, completed, description, dueDate, priority } = validatedFields.data;
-
 	try {
-		// const session = await auth();
+		const { authenticated, user } = await getAuthStatus();
 
-		// if (!session?.user?.id) {
-		// 	return {
-		// 		error: en.auth.UNAUTHENTICATED,
-		// 	};
-		// }
+		if (!authenticated) {
+			return {
+				error: en.auth.UNAUTHENTICATED,
+			};
+		}
 
-		// // if user specifies the course, we have to check if that course exists
-		// // if not, it doesn't matter because courseId is going to be null in the database
-		// if (courseId) {
-		// 	const course = await db.course.findUnique({
-		// 		where: { id: courseId, userId: session.user.id },
-		// 	});
+		const task = await db.task.create({
+			data: {
+				userId: user.id,
+				title,
+			},
+		});
 
-		// 	if (course && course?.userId != session.user.id) {
-		// 		return { error: en.courses.NOT_FOUND };
-		// 	}
-		// }
-
-		// const newTask = await db.task.create({
-		// 	data: {
-		// 		id,
-		// 		userId: session.user.id,
-		// 		courseId: courseId || null,
-		// 		title,
-		// 		completed,
-		// 		description,
-		// 		dueDate,
-		// 		priority,
-		// 	},
-		// });
-
-		// return { newTask };
-
-		return { newTask: null };
+		return { task };
 	} catch (error) {
 		return { error: en.SOMETHING_WENT_WRONG };
 	}
--- src/app/tasks/_components/create-task.tsx
+++ src/app/tasks/_components/create-task.tsx
@@ -1,98 +1,81 @@
 'use client';
 
+import FormLoadingSpinner from '@/components/common/form-loading-spinner';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Command, Plus } from 'lucide-react';
+import { useActionState, useEffect, useRef } from 'react';
+import { useIntersectionObserver } from 'usehooks-ts';
+import createTask from '../_actions/create-task';
+
 const CreateTask = () => {
-	// const { add, tasks } = useTasks();
-	// const { makeUpdate } = useTasksHistory();
-	// const [title, setTitle] = useState('');
-	// const inputRef = useRef<HTMLInputElement | null>(null);
-	// const { redo, undo } = useTasksHistory();
-	// // Create a new task and reset input state
-	// const handleSubmit = (e: React.FormEvent) => {
-	// 	e.preventDefault();
-	// 	const taskId = uid();
-	// 	const newTask = {
-	// 		id: taskId,
-	// 		title,
-	// 		description: '',
-	// 		courseId: null,
-	// 		dueDate: null,
-	// 		priority: null,
-	// 		completed: false,
-	// 	};
-	// 	// Update task in state and database
-	// 	add({
-	// 		...newTask,
-	// 	});
-	// 	// Push update to tasks history (Cmd + Z)
-	// 	makeUpdate({
-	// 		type: 'create',
-	// 		...newTask,
-	// 	});
-	// 	setTitle('');
-	// };
-	// const handleFocusInput = () => {
-	// 	if (!inputRef.current) return;
-	// 	inputRef.current.focus();
-	// };
-	// const { isIntersecting, ref: intersectorRef } = useIntersectionObserver();
-	// // Detect keyboard shortcut
-	// useEffect(() => {
-	// 	const listener = (e: KeyboardEvent) => {
-	// 		// Don't do anything if input is active
-	// 		if (inputRef.current === document.activeElement) return;
-	// 		if (e.metaKey && e.key.toLowerCase() === 'k') {
-	// 			handleFocusInput();
-	// 		} else if (e.metaKey && e.key.toLowerCase() === 'z') {
-	// 			e.preventDefault();
-	// 			if (e.shiftKey) {
-	// 				redo();
-	// 			} else {
-	// 				undo();
-	// 			}
-	// 		}
-	// 	};
-	// 	window.addEventListener('keydown', listener);
-	// 	if (tasks?.length === 0) {
-	// 		handleFocusInput();
-	// 	}
-	// 	return () => {
-	// 		window.removeEventListener('keydown', listener);
-	// 	};
-	// }, [tasks?.length, undo, redo]);
-	// return (
-	// 	<form
-	// 		ref={intersectorRef}
-	// 		className='z-20 space-y-4 rounded-2xl bg-white p-4 shadow-[0_0_32px_-20px_rgba(0,0,0,0.3)] dark:bg-neutral-700 sm:sticky sm:top-0 sm:p-8'
-	// 		onSubmit={handleSubmit}
-	// 		onClick={handleFocusInput}>
-	// 		<h2 className='text-xl font-bold sm:text-2xl'>
-	// 			Create a new task <br className='sm:hidden' />
-	// 			<span className='text-base'>
-	// 				(<Command className='mb-1 inline h-4 w-4' /> + K)
-	// 			</span>
-	// 		</h2>
-	// 		<div className='flex flex-col gap-4 sm:flex-row sm:gap-0'>
-	// 			<Input
-	// 				id='create-task-title'
-	// 				placeholder='Do the math homework'
-	// 				className='h-11 sm:rounded-r-none sm:border-r-transparent sm:text-base'
-	// 				value={title}
-	// 				onChange={e => setTitle(e.target.value)}
-	// 				ref={inputRef}
-	// 			/>
-	// 			<Button className='h-11 sm:rounded-l-none sm:text-base' type='submit' disabled={title.length === 0}>
-	// 				<Plus className='h-5 w-5' />
-	// 				Add to the list
-	// 			</Button>
-	// 		</div>
-	// 		{/* Should appear only on mobile when form is not visible */}
-	// 		{!isIntersecting && (
-	// 			<Button type='button' size='icon' className='fixed bottom-8 right-8 z-20 h-12 w-12 rounded-full'>
-	// 				<Plus />
-	// 			</Button>
-	// 		)}
-	// 	</form>
-	// );
+	const [_, formAction] = useActionState(createTask, null);
+
+	const inputRef = useRef<HTMLInputElement | null>(null);
+
+	const handleFocusInput = () => {
+		if (!inputRef.current) return;
+		inputRef.current.focus();
+	};
+	const { isIntersecting, ref: intersectorRef } = useIntersectionObserver();
+
+	// Detect keyboard shortcut
+	useEffect(() => {
+		const listener = (e: KeyboardEvent) => {
+			// Don't do anything if input is active
+			if (inputRef.current === document.activeElement) return;
+
+			// Focus input on Cmd + K
+			if (e.metaKey && e.key.toLowerCase() === 'k') {
+				handleFocusInput();
+			}
+		};
+		window.addEventListener('keydown', listener);
+
+		return () => {
+			window.removeEventListener('keydown', listener);
+		};
+	}, []);
+
+	return (
+		<aside
+			ref={intersectorRef}
+			className='z-20 space-y-4 rounded-2xl bg-white p-4 shadow-[0_0_32px_-20px_rgba(0,0,0,0.3)] dark:bg-neutral-700 sm:sticky sm:top-0 sm:p-8'
+			onClick={handleFocusInput}>
+			{/* Heading */}
+			<h2 className='text-xl font-bold sm:text-2xl'>
+				Create a new task <br className='sm:hidden' />
+				<span className='text-base'>
+					(<Command className='mb-1 inline h-4 w-4' /> + K)
+				</span>
+			</h2>
+
+			{/* Main form */}
+			<form action={formAction} className='flex flex-col gap-4 sm:flex-row sm:gap-0'>
+				<Input
+					id='create-task-title'
+					placeholder='Do the math homework'
+					className='h-11 sm:rounded-r-none sm:border-r-transparent sm:text-base'
+					aria-label='New task title'
+					name='title'
+					required
+					ref={inputRef}
+				/>
+				<Button className='h-11 sm:rounded-l-none sm:text-base' type='submit'>
+					<FormLoadingSpinner />
+					<Plus className='h-5 w-5' />
+					Add to the list
+				</Button>
+			</form>
+
+			{/* Should appear only on mobile when form is not visible */}
+			{!isIntersecting && (
+				<Button type='button' size='icon' className='fixed bottom-8 right-8 z-20 h-12 w-12 rounded-full'>
+					<Plus />
+				</Button>
+			)}
+		</aside>
+	);
 };
 
 export default CreateTask;
