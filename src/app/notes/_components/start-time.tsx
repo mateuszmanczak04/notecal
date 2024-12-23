e117c97c8d4da08f118a90a96e90248e1079779d
Mateusz Mańczak <mateuszmanczak@icloud.com>
2024-12-23 20:02:04 +0100

    Fix note's startTime

--- src/app/notes/_components/start-time.tsx
+++ src/app/notes/_components/start-time.tsx
@@ -1,14 +1,17 @@
 'use client';
 
 import DatePicker from '@/components/date-picker';
+import { cn } from '@/utils/cn';
 import { Note } from '@prisma/client';
+import { useTransition } from 'react';
+import updateNote from '../_actions/update-note';
 
 type Props = {
 	note: Note;
 };
 
 const StartTime = ({ note }: Props) => {
-	const updateNote = (input: any) => {};
+	const [isPending, startTransition] = useTransition();
 
 	const onChange = (newStartTime: Date | null) => {
 		if (!newStartTime) return;
@@ -16,10 +19,21 @@ const StartTime = ({ note }: Props) => {
 		// TODO: display a message telling you can't set it like that
 		if (newStartTime > note.endTime) return;
 
-		updateNote({ id: note.id, startTime: newStartTime });
+		startTransition(async () => {
+			await updateNote({ id: note.id, startTime: newStartTime });
+		});
 	};
 
-	return <DatePicker className='w-56' date={note.startTime} onSelect={newDate => onChange(newDate)} />;
+	return (
+		<article>
+			<p className='text-xl font-semibold'>Start time:</p>
+			<DatePicker
+				className={cn('mt-2 w-56', isPending && 'opacity-50')}
+				date={note.endTime}
+				onSelect={newDate => onChange(newDate)}
+			/>
+		</article>
+	);
 };
 
 export default StartTime;
