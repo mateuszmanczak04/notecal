f2f5930a147a9bcc30d0f547a3005a5b075aa46b
Mateusz Mańczak <mateuszmanczak@icloud.com>
2024-12-23 14:42:03 +0100

    Move user fetching logic to MainLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,10 @@
 import '@/app/globals.css';
 import MainLayout from '@/components/common/main-layout';
 import { getAuthStatus } from '@/lib/auth';
-import db from '@/lib/db';
 import { cn } from '@/lib/utils';
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import { Inter } from 'next/font/google';
-import { cookies } from 'next/headers';
-import { redirect } from 'next/navigation';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -16,33 +13,7 @@ export default async function RootLayout({
 }: Readonly<{
 	children: React.ReactNode;
 }>) {
-	const { authenticated, user: authUser } = await getAuthStatus();
-
-	if (!authenticated) {
-		return (
-			<html lang='en'>
-				<body
-					className={cn(
-						inter.className,
-						'bg-white fill-neutral-800 text-neutral-800 dark:bg-neutral-900 dark:fill-neutral-100 dark:text-neutral-100',
-					)}>
-					<div className='pt-16'>{children}</div>
-					<Analytics />
-					<SpeedInsights />
-				</body>
-			</html>
-		);
-	}
-
-	const user = await db.user.findUnique({
-		where: { id: authUser.id },
-	});
-
-	if (!user) {
-		// Should not occur in normal conditions
-		(await cookies()).delete('authToken');
-		redirect('/auth/login');
-	}
+	const { authenticated } = await getAuthStatus();
 
 	return (
 		<html lang='en'>
@@ -51,7 +22,8 @@ export default async function RootLayout({
 					inter.className,
 					'bg-neutral-100 fill-neutral-800 text-neutral-800 dark:bg-neutral-900 dark:fill-neutral-100 dark:text-neutral-100',
 				)}>
-				<MainLayout user={{ email: user.email }}>{children}</MainLayout>
+				{authenticated ? <MainLayout>{children}</MainLayout> : <div className='pt-16'>{children}</div>}
+
 				<Analytics />
 				<SpeedInsights />
 			</body>
--- src/components/common/main-layout.tsx
+++ src/components/common/main-layout.tsx
@@ -1,16 +1,23 @@
-'use client';
-
+import logout from '@/app/auth/_actions/logout';
 import Navigation from '@/components/common/navigation';
+import { getAuthStatus } from '@/lib/auth';
+import db from '@/lib/db';
 import React from 'react';
 
 type MainLayoutProps = {
 	children: React.ReactNode;
-	user: {
-		email: string;
-	};
 };
 
-const MainLayout = ({ children, user }: MainLayoutProps) => {
+const MainLayout = async ({ children }: MainLayoutProps) => {
+	// We assume user can't see this layout not being authenticated
+	const { user: authUser } = (await getAuthStatus()) as { user: { id: string } };
+
+	const user = await db.user.findUnique({
+		where: { id: authUser.id },
+	});
+
+	if (!user) return logout();
+
 	return (
 		<div className='flex h-screen overflow-y-hidden p-4 pl-12 xl:pl-4'>
 			<Navigation email={user.email} />
