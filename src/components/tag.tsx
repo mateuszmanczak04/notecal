a293732de8ce46eacce9d7c32409f5c9438d56d6
Mateusz Mańczak <mateuszmanczak@icloud.com>
2024-12-23 19:53:01 +0100

    Fix <Tag /> dark mode hover color

--- src/components/tag.tsx
+++ src/components/tag.tsx
@@ -12,7 +12,7 @@ const Tag: FC<TagProps> = ({ children, className, onClick }) => {
 		<div
 			onClick={onClick}
 			className={cn(
-				'flex h-6 max-w-52 cursor-pointer select-none items-center justify-center gap-2 text-nowrap rounded-xl bg-neutral-100 px-4 transition hover:bg-neutral-200 dark:bg-neutral-700',
+				'flex h-6 max-w-52 cursor-pointer select-none items-center justify-center gap-2 text-nowrap rounded-xl bg-neutral-100 px-4 transition hover:bg-neutral-200 dark:bg-neutral-700 dark:hover:bg-neutral-600',
 				className,
 			)}>
 			{children}
