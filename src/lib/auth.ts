96403456f84b2d5a932cc3b8fe7103a446bcf628
Mateusz Mańczak <mateuszmanczak@icloud.com>
2024-12-23 13:19:38 +0100

    Wrap getAuthStatus with React.cache

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,41 +1,44 @@
 'server-only';
 import { cookies } from 'next/headers';
+import { cache } from 'react';
 import { verifyToken } from './jwt';
 
 /**
  * Run this function on the server to check if user is authenticated. It's based on cookies and JWT.
  */
-export const getAuthStatus = async (): Promise<
-	| {
-			authenticated: true;
-			user: { id: string };
-	  }
-	| { authenticated: false; user: null }
-> => {
-	const cookieStore = await cookies();
+export const getAuthStatus = cache(
+	async (): Promise<
+		| {
+				authenticated: true;
+				user: { id: string };
+		  }
+		| { authenticated: false; user: null }
+	> => {
+		const cookieStore = await cookies();
 
-	const authToken = cookieStore.get('authToken')?.value;
+		const authToken = cookieStore.get('authToken')?.value;
 
-	if (!authToken) {
+		if (!authToken) {
+			return {
+				authenticated: false,
+				user: null,
+			};
+		}
+
+		const decoded = await verifyToken(authToken);
+
+		if (!decoded) {
+			return {
+				authenticated: false,
+				user: null,
+			};
+		}
+
 		return {
-			authenticated: false,
-			user: null,
+			authenticated: true,
+			user: {
+				id: decoded.id,
+			},
 		};
-	}
-
-	const decoded = await verifyToken(authToken);
-
-	if (!decoded) {
-		return {
-			authenticated: false,
-			user: null,
-		};
-	}
-
-	return {
-		authenticated: true,
-		user: {
-			id: decoded.id,
-		},
-	};
-};
+	},
+);
