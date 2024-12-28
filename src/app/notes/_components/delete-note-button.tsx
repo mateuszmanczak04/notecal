cb1abf67fca9f88b44233ddfd631d0dfb9d8845a
Mateusz Mańczak <mateuszmanczak@icloud.com>
2024-12-28 11:36:53 +0100

    Handle errors in delete-note-button

--- src/app/notes/_components/delete-note-button.tsx
+++ src/app/notes/_components/delete-note-button.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { Button } from '@/components/button';
+import { useToast } from '@/components/toast/use-toast';
 import { cn } from '@/utils/cn';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { Trash2 } from 'lucide-react';
@@ -17,9 +18,13 @@ type Props = {
  */
 const DeleteNoteButton = ({ id }: Props) => {
 	const queryClient = useQueryClient();
+	const { toast } = useToast();
 	const { mutate, isPending } = useMutation({
 		mutationFn: deleteNote,
-		onSuccess: () => {
+		onSettled: data => {
+			if (data && 'error' in data) {
+				toast({ description: data.error, variant: 'destructive' });
+			}
 			queryClient.invalidateQueries({ queryKey: ['notes'] });
 		},
 	});
