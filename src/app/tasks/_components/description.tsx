eb998735cbe4bd7e3840f958a22f3b586b371963
Mateusz Mańczak <mateuszmanczak@icloud.com>
2024-12-28 11:39:51 +0100

    Handle errors in description

--- src/app/tasks/_components/description.tsx
+++ src/app/tasks/_components/description.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { useToast } from '@/components/toast/use-toast';
 import { cn } from '@/utils/cn';
 import { Task } from '@prisma/client';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
@@ -14,9 +15,13 @@ const Description = ({ task }: Props) => {
 	const { id, description } = task;
 	const queryClient = useQueryClient();
 	const descriptionRef = useRef<HTMLParagraphElement>(null!);
+	const { toast } = useToast();
 	const { mutate, isPending } = useMutation({
 		mutationFn: updateTask,
-		onSuccess: () => {
+		onSettled: data => {
+			if (data && 'error' in data) {
+				toast({ description: data.error, variant: 'destructive' });
+			}
 			queryClient.invalidateQueries({ queryKey: ['tasks'] });
 		},
 	});
