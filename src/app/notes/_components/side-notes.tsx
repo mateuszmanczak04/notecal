7ac063d737b3b79547f2d5941030db5ece05aec4
Mateusz Mańczak <mateuszmanczak@icloud.com>
2024-12-28 11:37:36 +0100

    Handle errors in side-notes

--- src/app/notes/_components/side-notes.tsx
+++ src/app/notes/_components/side-notes.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { Button } from '@/components/button';
+import { useToast } from '@/components/toast/use-toast';
 import { cn } from '@/utils/cn';
 import { Course, Note } from '@prisma/client';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
@@ -17,9 +18,13 @@ type Props = {
 
 const SideNotes = ({ currentCourse, currentNoteId, currentCourseNotes }: Props) => {
 	const queryClient = useQueryClient();
+	const { toast } = useToast();
 	const { mutate, isPending } = useMutation({
 		mutationFn: createNote,
-		onSuccess: () => {
+		onSettled: data => {
+			if (data && 'error' in data) {
+				toast({ description: data.error, variant: 'destructive' });
+			}
 			queryClient.invalidateQueries({ queryKey: ['notes'] });
 		},
 	});
