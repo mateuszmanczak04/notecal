0024576f50f3b0a437c3a4054be533f92668afc0
Mateusz Mańczak <mateuszmanczak@icloud.com>
2025-01-12 18:36:09 +0100

    feat: add current sort criteria to sort-tasks

--- src/app/tasks/_components/sort-tasks.tsx
+++ src/app/tasks/_components/sort-tasks.tsx
@@ -3,12 +3,14 @@
 import updateSettings from '@/app/settings/_actions/update-settings';
 import { DropdownMenu, DropdownMenuItem, DropdownMenuList, DropdownMenuTrigger } from '@/components/dropdown-menu';
 import { useToast } from '@/components/toast/use-toast';
+import { useUser } from '@/hooks/use-user';
 import { cn } from '@/utils/cn';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 const SortTasks = () => {
 	const { toast } = useToast();
 	const queryClient = useQueryClient();
+	const { data: user } = useUser();
 	const { mutate, isPending } = useMutation({
 		mutationFn: updateSettings,
 		onSettled: data => {
@@ -32,9 +34,31 @@ const SortTasks = () => {
 			mutate({ orderTasks: newCriteria });
 		}
 	};
+
+	const getNameOfCriteria = (criteria: string) => {
+		if (criteria === 'title') {
+			return 'Title';
+		}
+		if (criteria === 'createdAt') {
+			return 'Newest first';
+		}
+		if (criteria === 'dueDate') {
+			return 'Due date';
+		}
+		if (criteria === 'priority') {
+			return 'Priority';
+		}
+		if (criteria === 'completed') {
+			return 'Completed';
+		}
+		return '';
+	};
+
+	if (!user) return;
+
 	return (
 		<DropdownMenu className={cn('relative flex-1', isPending && 'opacity-50')}>
-			<DropdownMenuTrigger showChevron>Order by</DropdownMenuTrigger>
+			<DropdownMenuTrigger showChevron>Order by ({getNameOfCriteria(user.orderTasks)})</DropdownMenuTrigger>
 			<DropdownMenuList>
 				<DropdownMenuItem value='title' onSelect={handleSort}>
 					Title
