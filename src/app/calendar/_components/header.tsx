c30a5647e03c1b8c9c5c01ce944259ad8ec2a8c7
Mateusz Mańczak <mateuszmanczak@icloud.com>
2025-01-13 16:59:33 +0100

    feat: hide zoom in/out buttons in month view

--- src/app/calendar/_components/header.tsx
+++ src/app/calendar/_components/header.tsx
@@ -75,22 +75,24 @@ const Header = () => {
 				</div>
 
 				{/* Zoom in/out */}
-				<div className='flex h-calendar-header w-20'>
-					<button
-						className='flex flex-1 cursor-pointer items-center justify-center rounded-l-md border hover:bg-neutral-100 dark:border-neutral-600 dark:hover:bg-neutral-700'
-						aria-label='zoom out'
-						title='zoom out'
-						onClick={zoomOut}>
-						<Minus />
-					</button>
-					<button
-						className='flex flex-1 cursor-pointer items-center justify-center rounded-r-md border-b border-r border-t hover:bg-neutral-100 dark:border-neutral-600 dark:hover:bg-neutral-700'
-						aria-label='zoom in'
-						title='zoom in'
-						onClick={zoomIn}>
-						<Plus />
-					</button>
-				</div>
+				{viewMode === 'days' && (
+					<div className='flex h-calendar-header w-20'>
+						<button
+							className='flex flex-1 cursor-pointer items-center justify-center rounded-l-md border hover:bg-neutral-100 dark:border-neutral-600 dark:hover:bg-neutral-700'
+							aria-label='zoom out'
+							title='zoom out'
+							onClick={zoomOut}>
+							<Minus />
+						</button>
+						<button
+							className='flex flex-1 cursor-pointer items-center justify-center rounded-r-md border-b border-r border-t hover:bg-neutral-100 dark:border-neutral-600 dark:hover:bg-neutral-700'
+							aria-label='zoom in'
+							title='zoom in'
+							onClick={zoomIn}>
+							<Plus />
+						</button>
+					</div>
+				)}
 			</div>
 		</div>
 	);
