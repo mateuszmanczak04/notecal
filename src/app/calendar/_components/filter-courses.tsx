a93b2fb1d556800695df2d0b19c3c01de536461f
Mateusz Mańczak <mateuszmanczak@icloud.com>
2025-01-13 14:39:53 +0100

    feat: handle filtering courses in filter-courses component

--- src/app/calendar/_components/filter-courses.tsx
+++ src/app/calendar/_components/filter-courses.tsx
@@ -2,11 +2,21 @@
 
 import { Checkbox } from '@/components/checkbox';
 import { useCourses } from '@/hooks/use-courses';
+import { useCalendarContext } from '../_context/calendar-context';
 
 type Props = {};
 
 const FilterCourses = ({}: Props) => {
 	const { data: courses } = useCourses();
+	const { handleHideCourse, handleShowCourse } = useCalendarContext();
+
+	const handleChange = (checked: boolean | string, id: string) => {
+		if (checked) {
+			handleShowCourse(id);
+		} else {
+			handleHideCourse(id);
+		}
+	};
 
 	if (!courses) return;
 
@@ -15,8 +25,12 @@ const FilterCourses = ({}: Props) => {
 			<p className='text-nowrap font-semibold'>Filter courses:</p>
 			<div className='flex items-center gap-4'>
 				{courses.map(course => (
-					<div key={course.id} className='flex items-center gap-1'>
-						<Checkbox id={course.id + '-filter'} className='size-5' />
+					<div key={course.id} className='flex select-none items-center gap-1'>
+						<Checkbox
+							id={course.id + '-filter'}
+							className='size-5'
+							onCheckedChange={checked => handleChange(checked, course.id)}
+						/>
 						<label htmlFor={course.id + '-filter'} className='cursor-pointer'>
 							{course.name}
 						</label>
