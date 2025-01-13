ffba7e25ed3a5c9e4b328a23fe19e8a2436395dc
Mateusz Mańczak <mateuszmanczak@icloud.com>
2025-01-13 12:11:18 +0100

    feat: note title component

--- src/app/notes/_components/note-title.tsx
+++ src/app/notes/_components/note-title.tsx
@@ -1,17 +1,71 @@
 'use client';
 
+import { useToast } from '@/components/toast/use-toast';
+import { cn } from '@/utils/cn';
 import { Note } from '@prisma/client';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useEffect, useRef } from 'react';
+import updateNote from '../_actions/update-note';
 
 type Props = {
 	note: Note;
 };
 
 const NoteTitle = ({ note }: Props) => {
+	const queryClient = useQueryClient();
+	const titleRef = useRef<HTMLHeadingElement>(null!);
+	const { toast } = useToast();
+	const { mutate, isPending } = useMutation({
+		mutationFn: updateNote,
+		onSettled: data => {
+			if (data && 'error' in data) {
+				toast({ description: data.error, variant: 'destructive' });
+			}
+			queryClient.invalidateQueries({ queryKey: ['notes'] });
+		},
+	});
+
+	const handleSubmit = () => {
+		const newTitle = titleRef.current.innerText;
+		// Don't want to update the same value:
+		if (newTitle.trim() === note.title) return;
+		mutate({ id: note.id, title: newTitle.trim() });
+	};
+
+	/**
+	 * Detect Enter and Escape keys for submission or cancellation.
+	 */
+	const handleKeyDown = (event: React.KeyboardEvent<HTMLParagraphElement>) => {
+		if (!titleRef.current) return;
+		if (event.key === 'Enter' && !event.shiftKey) {
+			event.preventDefault();
+			titleRef.current.blur(); // It automatically triggers handleSubmit()
+			return;
+		}
+		if (event.key === 'Escape') {
+			titleRef.current.innerText = note.title;
+			titleRef.current.blur();
+			return;
+		}
+	};
+
+	// Set initial value:
+	useEffect(() => {
+		if (!titleRef.current) return;
+		titleRef.current.innerText = note.title;
+	}, [note.title]);
+
 	return (
-		<div>
-			Title:
-			{note.title}
-		</div>
+		<h2
+			ref={titleRef}
+			contentEditable
+			className={cn(
+				'mb-2 break-all rounded-md bg-white px-3 py-2 text-center text-2xl font-semibold outline-none transition-opacity dark:bg-gray-800',
+				isPending && 'pointer-events-none opacity-50',
+			)}
+			onKeyDown={handleKeyDown}
+			onBlur={handleSubmit}
+			spellCheck={false}></h2>
 	);
 };
 
