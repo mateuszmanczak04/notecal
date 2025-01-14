ab79283c3eaea9992a02723a939dc8c3f633ae19
Mateusz Mańczak <mateuszmanczak@icloud.com>
2025-01-14 12:29:08 +0100

    feat(calendar): display notes in list as links

--- src/app/calendar/_components/list-view.tsx
+++ src/app/calendar/_components/list-view.tsx
@@ -1,11 +1,33 @@
 'use client';
 
+import { useCourses } from '@/hooks/use-courses';
 import { useNotes } from '@/hooks/use-notes';
+import { ChevronRight } from 'lucide-react';
+import Link from 'next/link';
 
 const ListView = () => {
 	const { data: notes } = useNotes();
+	const { data: courses } = useCourses();
 
-	return <div>{notes && notes.map(note => <div key={note.id}>{note.title}</div>)}</div>;
+	return (
+		<div className=' mt-4 flex flex-col items-center gap-2'>
+			{notes &&
+				courses &&
+				notes.map(note => {
+					const course = courses.find(c => c.id === note.courseId);
+					return (
+						<Link
+							href={`/notes/${note.id}`}
+							className='flex w-[clamp(240px,50%,800px)] items-center justify-between gap-4 rounded-xl p-4 hover:opacity-90'
+							style={{ backgroundColor: course?.color }}
+							key={note.id}>
+							<p className='font-semibold'>{note.title || course?.name}</p>
+							<ChevronRight className='size-7 shrink-0' />
+						</Link>
+					);
+				})}
+		</div>
+	);
 };
 
 export default ListView;
