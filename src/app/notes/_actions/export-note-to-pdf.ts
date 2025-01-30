f30a8c2bcc8384e6e8a320a9ad87f77b790873a4
Mateusz Mańczak <mateuszmanczak@icloud.com>
2025-01-30 16:01:57 +0100

    fix: spacings in pdf

--- src/app/notes/_actions/export-note-to-pdf.ts
+++ src/app/notes/_actions/export-note-to-pdf.ts
@@ -31,7 +31,7 @@ export const exportNoteToPDF = async ({
             <style>
                 @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap');
             </style>
-            ${fileTitle && `<small>${fileTitle} ${date && `,${date}`}</small>`}
+            ${fileTitle && `<p class='title'>${fileTitle} ${date && `,${date}`}</p>`}
             ${htmlContent}
         `);
 
@@ -46,11 +46,15 @@ export const exportNoteToPDF = async ({
 			});
 		}
 
-		await page.addStyleTag({ content: '* { font-family: "Inter", sans-serif; }' });
+		await page.addStyleTag({
+			content: '* { font-family: "Inter", sans-serif; box-sizing: border-box; margin: 0; padding: 0; }',
+		});
 		await page.addStyleTag({ content: 'h1 { font-size: 24px; line-height: 32px; }' });
 		await page.addStyleTag({ content: 'h2 { font-size: 20px; line-height: 28px; }' });
 		await page.addStyleTag({ content: 'p { font-size: 16px; line-height: 32px; }' });
-		await page.addStyleTag({ content: '.underline { text-decoration: underline; } ' });
+		await page.addStyleTag({ content: '.underline { text-decoration: underline; }' });
+		await page.addStyleTag({ content: '.title { margin-bottom: 16px; font-size: 14px; }' });
+
 		const pdfBuffer = await page.pdf({
 			format: 'A4',
 			printBackground: true,
