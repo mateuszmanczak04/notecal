fbdb35dc7fcd8649df41a8827c640f57bca9a1a3
Mateusz Mańczak <mateuszmanczak@icloud.com>
2025-01-31 15:31:31 +0100

    feat: don't show current note course in ChangeCourse

--- src/app/notes/_components/change-course.tsx
+++ src/app/notes/_components/change-course.tsx
@@ -48,19 +48,23 @@ const ChangeCourse = ({ currentCourse, note, handleClose }: Props) => {
 		<div className='flex flex-col justify-center overflow-clip rounded-xl border bg-white text-neutral-800 dark:border-neutral-500 dark:bg-neutral-600 dark:text-white'>
 			{/* Options */}
 			{courses &&
-				courses.map(course => (
-					<button
-						onClick={() => {
-							handleSelect(course.id);
-							handleClose && handleClose();
-						}}
-						key={course.id}
-						value={course.id}
-						className='flex h-9 cursor-pointer select-none items-center gap-2 truncate text-nowrap px-4 font-medium transition hover:bg-neutral-100 dark:hover:bg-neutral-500'>
-						<div className='h-3 w-3 shrink-0 rounded-full' style={{ backgroundColor: course.color }}></div>
-						<p className={'truncate text-sm'}>{course.name}</p>
-					</button>
-				))}
+				courses
+					.filter(c => c.id !== currentCourse.id)
+					.map(course => (
+						<button
+							onClick={() => {
+								handleSelect(course.id);
+								handleClose && handleClose();
+							}}
+							key={course.id}
+							value={course.id}
+							className='flex h-9 cursor-pointer select-none items-center gap-2 truncate text-nowrap px-4 font-medium transition hover:bg-neutral-100 dark:hover:bg-neutral-500'>
+							<div
+								className='h-3 w-3 shrink-0 rounded-full'
+								style={{ backgroundColor: course.color }}></div>
+							<p className={'truncate text-sm'}>{course.name}</p>
+						</button>
+					))}
 		</div>
 	);
 };
