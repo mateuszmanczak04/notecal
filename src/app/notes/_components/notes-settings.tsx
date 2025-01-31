e8f7999f6e168666aa88bd321797c34de89bef56
Mateusz Mańczak <mateuszmanczak@icloud.com>
2025-01-31 16:05:59 +0100

    chore: move default note duration setting to /notes page

--- src/app/notes/_components/notes-settings.tsx
+++ src/app/notes/_components/notes-settings.tsx
@@ -1,17 +1,21 @@
 'use client';
 
 import { Checkbox } from '@/components/checkbox';
+import { Input } from '@/components/input';
 import { useSettings } from '@/hooks/use-settings';
 
 const NotesSettings = () => {
-	const { noteAutoSave, setNoteAutoSave } = useSettings();
+	const { noteAutoSave, setNoteAutoSave, defaultNoteDuration, setDefaultNoteDuration } = useSettings();
 
 	return (
-		<fieldset className='space-y-2 rounded-xl border border-neutral-200 p-4 pt-0 dark:border-neutral-700'>
+		<fieldset className=' rounded-xl border border-neutral-200 p-4 pt-0 dark:border-neutral-700'>
 			<legend className='px-2'>Settings</legend>
+			<p className='mt-2 text-balance text-sm opacity-75'>
+				Note that these settings are reflected in all your notes, not only for this course
+			</p>
 
 			{/* Autosave */}
-			<div className='flex select-none items-center'>
+			<div className='mt-4 flex select-none items-center'>
 				<Checkbox
 					id='autosave-time'
 					name='autosave-time'
@@ -21,6 +25,20 @@ const NotesSettings = () => {
 				<label htmlFor='autosave-time' className='block flex-1 cursor-pointer pl-2 font-medium'>
 					Auto save content (every 30s)
 				</label>
+			</div>
+
+			{/* Default note duration */}
+			<div className='mt-4 select-none'>
+				<label htmlFor='default-note-duration' className='mb-1 block px-2 font-medium '>
+					Default new note duration (min)
+				</label>
+				<Input
+					placeholder='60'
+					type='tel'
+					id='default-note-duration'
+					value={defaultNoteDuration}
+					onChange={e => setDefaultNoteDuration(parseInt(e.target.value) || 0)}
+				/>
 			</div>
 		</fieldset>
 	);
--- src/app/settings/_components/update-settings.tsx
+++ src/app/settings/_components/update-settings.tsx
@@ -7,27 +7,13 @@ import { T_Language, useSettings } from '@/hooks/use-settings';
  * A component to update user's local settings
  */
 const UpdateSettings = () => {
-	const { defaultNoteDuration, setDefaultNoteDuration, language, setLanguage } = useSettings();
+	const { language, setLanguage } = useSettings();
 
 	return (
 		<section className='flex flex-col gap-2'>
 			<h2 className='text-lg font-semibold'>Update your settings</h2>
 
 			<div className='mt-2 flex flex-col gap-4'>
-				{/* Default note duration */}
-				<div>
-					<label htmlFor='update-default-note-duration' className='mb-1 block px-2 font-medium'>
-						Default note duration
-					</label>
-					<Input
-						placeholder='60'
-						type='number'
-						id='update-default-note-duration'
-						value={defaultNoteDuration}
-						onChange={e => setDefaultNoteDuration(parseInt(e.target.value))}
-					/>
-				</div>
-
 				{/* Language */}
 				<div>
 					<label htmlFor='update-language' className='mb-1 block px-2 font-medium'>
