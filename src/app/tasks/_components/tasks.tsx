f93d2ab82ee5bd57be0e956c263641502e27e6be
Mateusz Mańczak <mateuszmanczak@icloud.com>
2025-02-01 11:06:55 +0100

    ref: move note drag functionality into separate hook

--- src/app/tasks/_components/tasks.tsx
+++ src/app/tasks/_components/tasks.tsx
@@ -2,11 +2,8 @@
 
 import ErrorMessage from '@/components/error-message';
 import LoadingSpinner from '@/components/loading-spinner';
-import { useToast } from '@/components/toast/use-toast';
 import { useTasks } from '@/hooks/use-tasks';
-import { useMutation, useQueryClient } from '@tanstack/react-query';
-import { useRef, useState } from 'react';
-import updateTask from '../_actions/update-task';
+import { useTasksDrag } from '../hooks/use-tasks-drag';
 import Task from './task';
 
 /**
@@ -14,16 +11,8 @@ import Task from './task';
  */
 const Tasks = () => {
 	const { data: tasks, error, isPending } = useTasks();
-	const queryClient = useQueryClient();
-	const { toast } = useToast();
-	const { mutate } = useMutation({
-		mutationFn: updateTask,
-		onSettled: data => {
-			if (data && 'error' in data) {
-				toast({ description: data.error, variant: 'destructive' });
-			}
-			queryClient.invalidateQueries({ queryKey: ['tasks'] });
-		},
+	const { handleDragEnter, handleDragOver, handleDragStart, handleDrop, droppedTaskId } = useTasksDrag({
+		tasks: tasks || [],
 	});
 
 	if (isPending) return <LoadingSpinner />;
@@ -34,42 +23,8 @@ const Tasks = () => {
 		return <p className='text-center text-lg text-neutral-500 sm:ml-8'>You don&apos;t have any tasks yet.</p>;
 	}
 
-	const containerRef = useRef<HTMLDivElement>(null!);
-	const [draggedTaskId, setDraggedTaskId] = useState<string | null>(null);
-	const [droppedTaskId, setDroppedTaskId] = useState<string | null>(null);
-
-	const handleDragStart = (task: Task, e: React.DragEvent) => {
-		setDraggedTaskId(task.id);
-	};
-
-	const handleDragEnter = (task: Task, e: React.DragEvent) => {
-		if (!draggedTaskId) return;
-		setDroppedTaskId(task.id);
-	};
-
-	// Necessary for handleDrop to work
-	const handleDragOver = (e: React.DragEvent) => {
-		e.preventDefault();
-	};
-
-	const handleDrop = (task: Task, e: React.DragEvent) => {
-		if (!draggedTaskId || !droppedTaskId) return;
-
-		const droppedTaskIndex = tasks.findIndex(task => task.id === droppedTaskId);
-		const droppedTask = tasks[droppedTaskIndex];
-		const taskAfterDroppedTask = tasks[droppedTaskIndex + 1];
-		if (!taskAfterDroppedTask) {
-			mutate({ id: draggedTaskId, weight: droppedTask.weight + 10000 });
-		} else {
-			mutate({ id: draggedTaskId, weight: (droppedTask.weight + taskAfterDroppedTask.weight) / 2 });
-		}
-
-		setDraggedTaskId(null);
-		setDroppedTaskId(null);
-	};
-
 	return (
-		<div className='space-y-4' ref={containerRef}>
+		<div className='space-y-4'>
 			{tasks.map(task => (
 				<Task
 					isBeingDropped={task.id === droppedTaskId}
---
+++ src/app/tasks/hooks/use-tasks-drag.ts
@@ -0,0 +1,58 @@
+import { useToast } from '@/components/toast/use-toast';
+import { Task } from '@prisma/client';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useState } from 'react';
+import updateTask from '../_actions/update-task';
+
+type T_Props = {
+	tasks: Task[];
+};
+
+export const useTasksDrag = ({ tasks }: T_Props) => {
+	const queryClient = useQueryClient();
+	const { toast } = useToast();
+	const { mutate } = useMutation({
+		mutationFn: updateTask,
+		onSettled: data => {
+			if (data && 'error' in data) {
+				toast({ description: data.error, variant: 'destructive' });
+			}
+			queryClient.invalidateQueries({ queryKey: ['tasks'] });
+		},
+	});
+
+	const [draggedTaskId, setDraggedTaskId] = useState<string | null>(null);
+	const [droppedTaskId, setDroppedTaskId] = useState<string | null>(null);
+
+	const handleDragStart = (task: Task, e: React.DragEvent) => {
+		setDraggedTaskId(task.id);
+	};
+
+	const handleDragEnter = (task: Task, e: React.DragEvent) => {
+		if (!draggedTaskId) return;
+		setDroppedTaskId(task.id);
+	};
+
+	// Necessary for handleDrop to work
+	const handleDragOver = (e: React.DragEvent) => {
+		e.preventDefault();
+	};
+
+	const handleDrop = (task: Task, e: React.DragEvent) => {
+		if (!draggedTaskId || !droppedTaskId) return;
+
+		const droppedTaskIndex = tasks.findIndex(task => task.id === droppedTaskId);
+		const droppedTask = tasks[droppedTaskIndex];
+		const taskAfterDroppedTask = tasks[droppedTaskIndex + 1];
+		if (!taskAfterDroppedTask) {
+			mutate({ id: draggedTaskId, weight: droppedTask.weight + 10000 });
+		} else {
+			mutate({ id: draggedTaskId, weight: (droppedTask.weight + taskAfterDroppedTask.weight) / 2 });
+		}
+
+		setDraggedTaskId(null);
+		setDroppedTaskId(null);
+	};
+
+	return { handleDragStart, handleDragEnter, handleDragOver, handleDrop, droppedTaskId };
+};
