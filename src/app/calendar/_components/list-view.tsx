6fa972d34daf64aab04e8a658522a054eae372f3
Mateusz Mańczak <mateuszmanczak@icloud.com>
2025-02-05 16:26:12 +0100

    feat: filtering courses in calendar list view mode

--- src/app/calendar/_components/list-view.tsx
+++ src/app/calendar/_components/list-view.tsx
@@ -7,67 +7,69 @@ import { useNotesWithTime } from '@/hooks/use-notes-with-time';
 import { format } from 'date-fns';
 import { ChevronRight } from 'lucide-react';
 import Link from 'next/link';
+import { useCalendarContext } from '../_context/calendar-context';
 
 const ListView = () => {
 	const { data: notesWithoutTime } = useNotes();
 	const { data: notesWithTime } = useNotesWithTime();
 	const { data: courses } = useCourses();
+	const { hiddenCoursesIds } = useCalendarContext();
+
+	const filteredNotesWithTime = notesWithTime.filter(n => !hiddenCoursesIds.includes(n.courseId));
+	const filteredNotesWithoutTime = notesWithoutTime?.filter(n => !hiddenCoursesIds.includes(n.courseId));
+
+	if (!courses || !notesWithoutTime) return;
 
 	return (
 		<div className=' mt-4 flex flex-col items-center gap-2'>
 			{/* Notes without time at the top */}
-			{notesWithoutTime &&
-				courses &&
-				notesWithoutTime.map(note => {
-					const course = courses.find(c => c.id === note.courseId);
-					return (
-						<Link
-							href={`/notes?noteId=${note.id}`}
-							className='flex w-[clamp(240px,100%,800px)] items-center justify-between gap-4 rounded-xl p-4 text-white hover:opacity-90'
-							style={{ backgroundColor: course?.color }}
-							key={note.id}>
-							<div className='min-w-0'>
-								{course?.name && <p className='mt-2 truncate text-lg'>{course.name}</p>}
-								{note.title && <p className='mt-2 truncate text-lg font-semibold'>{note.title}</p>}
-								{note.content && (
-									<p className='mt-2 line-clamp-4 w-full whitespace-pre-line text-sm opacity-75'>
-										{parseLexicalJsonToPlainText(JSON.parse(note.content))}
-									</p>
-								)}
-							</div>
-							<ChevronRight className='size-7 shrink-0' />
-						</Link>
-					);
-				})}
+			{filteredNotesWithoutTime?.map(note => {
+				const course = courses.find(c => c.id === note.courseId);
+				return (
+					<Link
+						href={`/notes?noteId=${note.id}`}
+						className='flex w-[clamp(240px,100%,800px)] items-center justify-between gap-4 rounded-xl p-4 text-white hover:opacity-90'
+						style={{ backgroundColor: course?.color }}
+						key={note.id}>
+						<div className='min-w-0'>
+							{course?.name && <p className='mt-2 truncate text-lg'>{course.name}</p>}
+							{note.title && <p className='mt-2 truncate text-lg font-semibold'>{note.title}</p>}
+							{note.content && (
+								<p className='mt-2 line-clamp-4 w-full whitespace-pre-line text-sm opacity-75'>
+									{parseLexicalJsonToPlainText(JSON.parse(note.content))}
+								</p>
+							)}
+						</div>
+						<ChevronRight className='size-7 shrink-0' />
+					</Link>
+				);
+			})}
 
 			{/* Notes with time */}
-			{notesWithTime &&
-				courses &&
-				notesWithTime.map(note => {
-					const course = courses.find(c => c.id === note.courseId);
-					return (
-						<Link
-							href={`/notes?noteId=${note.id}`}
-							className='flex w-[clamp(240px,100%,800px)] items-center justify-between gap-4 rounded-xl p-4 text-white hover:opacity-90'
-							style={{ backgroundColor: course?.color }}
-							key={note.id}>
-							<div className='min-w-0'>
-								<div className='flex gap-2'>
-									<p>{format(note.startTime, 'P, HH:MM')}</p>-
-									<p>{format(note.endTime, 'P, HH:MM')}</p>
-								</div>
-								{course?.name && <p className='mt-2 truncate text-lg'>{course.name}</p>}
-								{note.title && <p className='mt-2 truncate text-lg font-semibold'>{note.title}</p>}
-								{note.content && (
-									<p className='mt-2 line-clamp-4 w-full whitespace-pre-line text-sm opacity-75'>
-										{parseLexicalJsonToPlainText(JSON.parse(note.content))}
-									</p>
-								)}
+			{filteredNotesWithTime?.map(note => {
+				const course = courses.find(c => c.id === note.courseId);
+				return (
+					<Link
+						href={`/notes?noteId=${note.id}`}
+						className='flex w-[clamp(240px,100%,800px)] items-center justify-between gap-4 rounded-xl p-4 text-white hover:opacity-90'
+						style={{ backgroundColor: course?.color }}
+						key={note.id}>
+						<div className='min-w-0'>
+							<div className='flex gap-2'>
+								<p>{format(note.startTime, 'P, HH:MM')}</p>-<p>{format(note.endTime, 'P, HH:MM')}</p>
 							</div>
-							<ChevronRight className='size-7 shrink-0' />
-						</Link>
-					);
-				})}
+							{course?.name && <p className='mt-2 truncate text-lg'>{course.name}</p>}
+							{note.title && <p className='mt-2 truncate text-lg font-semibold'>{note.title}</p>}
+							{note.content && (
+								<p className='mt-2 line-clamp-4 w-full whitespace-pre-line text-sm opacity-75'>
+									{parseLexicalJsonToPlainText(JSON.parse(note.content))}
+								</p>
+							)}
+						</div>
+						<ChevronRight className='size-7 shrink-0' />
+					</Link>
+				);
+			})}
 		</div>
 	);
 };
