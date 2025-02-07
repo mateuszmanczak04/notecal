b6f4709011ee071463a831b904d849e62a23e724
Mateusz Mańczak <mateuszmanczak@icloud.com>
2025-02-07 22:02:16 +0100

    style: update note settings

--- src/app/notes/_components/sidebar/notes-settings.tsx
+++ src/app/notes/_components/sidebar/notes-settings.tsx
@@ -8,14 +8,14 @@ const NotesSettings = () => {
 	const { noteAutoSave, setNoteAutoSave, defaultNoteDuration, setDefaultNoteDuration } = useSettings();
 
 	return (
-		<fieldset className=' rounded-xl border border-neutral-200 p-4 pt-0 dark:border-neutral-700'>
-			<legend className='px-2'>Settings</legend>
-			<p className='mt-2 text-balance text-sm opacity-75'>
+		<div className='border-b border-neutral-200 pb-4 dark:border-neutral-700'>
+			<p className='px-2 font-semibold'>Settings</p>
+			<p className='mt-2 text-balance px-2 text-sm opacity-75'>
 				Note that these settings are reflected in all your notes, not only for this course
 			</p>
 
 			{/* Autosave */}
-			<div className='mt-4 flex select-none items-center'>
+			<div className='mt-6 flex select-none items-center px-2'>
 				<Checkbox
 					id='autosave-time'
 					name='autosave-time'
@@ -28,7 +28,7 @@ const NotesSettings = () => {
 			</div>
 
 			{/* Default note duration */}
-			<div className='mt-4 select-none'>
+			<div className='mt-6 select-none'>
 				<label htmlFor='default-note-duration' className='mb-1 block px-2 font-medium '>
 					Default new note duration (min)
 				</label>
@@ -40,7 +40,7 @@ const NotesSettings = () => {
 					onChange={e => setDefaultNoteDuration(parseInt(e.target.value) || 0)}
 				/>
 			</div>
-		</fieldset>
+		</div>
 	);
 };
 
