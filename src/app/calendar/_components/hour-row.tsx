556857a49bc93c0d5f77ea5e07d3f0dd2b3b4d0b
Mateusz Mańczak <mateuszmanczak@icloud.com>
2025-02-07 15:40:17 +0100

    style: remove rounded corner

--- src/app/calendar/_components/hour-row.tsx
+++ src/app/calendar/_components/hour-row.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { useSettings } from '@/hooks/use-settings';
-import { cn } from '@/utils/cn';
 import { getCalendarRowHeight } from '../_utils/get-calendar-row-height';
 
 const HourRow = ({ hour }: { hour: number }) => {
@@ -9,10 +8,7 @@ const HourRow = ({ hour }: { hour: number }) => {
 
 	return (
 		<div
-			className={cn(
-				'flex flex-col items-center justify-center border-b border-l border-r font-semibold text-neutral-500 transition-[height] sm:flex-row dark:border-neutral-600 dark:text-neutral-400',
-				hour === 23 && 'rounded-bl-xl',
-			)}
+			className='flex flex-col items-center justify-center border-b border-l border-r font-semibold text-neutral-500 transition-[height] sm:flex-row dark:border-neutral-600 dark:text-neutral-400'
 			style={{ height: getCalendarRowHeight({ zoomLevel }) + 'px' }}>
 			<span className=''>{hour.toString().padStart(2, '00')} </span>
 			<span className='hidden sm:inline'>:00</span>
