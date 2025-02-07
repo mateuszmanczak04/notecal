18f89f8359d4f050818936790bcc3943ee1e79a9
Mateusz Mańczak <mateuszmanczak@icloud.com>
2025-02-07 15:46:47 +0100

    style: update style of NoNoteContent

--- src/app/notes/_components/no-note-content.tsx
+++ src/app/notes/_components/no-note-content.tsx
@@ -10,6 +10,7 @@ import {
 	Check,
 	ChevronLeft,
 	ChevronRight,
+	FileOutput,
 	Heading1,
 	Heading2,
 	Italic,
@@ -25,11 +26,9 @@ type T_Props = {
  */
 const NoNoteContent = ({ course }: T_Props) => {
 	return (
-		<article className='flex h-full flex-1 flex-col bg-neutral-100 p-4 dark:bg-neutral-700'>
-			<h2 className='mb-2 break-all rounded-md bg-white px-3 py-2 text-center text-2xl font-semibold text-opacity-50 outline-none transition-opacity dark:bg-neutral-800'>
-				Please select or create a note to continue
-			</h2>
-			<div className='flex flex-wrap items-center justify-center gap-2 rounded-md bg-white p-2 dark:bg-neutral-800'>
+		<article className='flex h-full flex-1 flex-col bg-white dark:bg-neutral-700'>
+			{/* Toolbar */}
+			<div className='flex flex-wrap items-center gap-2 border-b border-neutral-200 bg-white p-2 dark:border-neutral-600 dark:bg-neutral-700'>
 				{/* Undo & Redo */}
 				<div className='grid grid-cols-2 gap-1 rounded-md bg-neutral-100 dark:bg-neutral-700'>
 					<Toggle title='Ctrl + Z'>
@@ -79,13 +78,25 @@ const NoNoteContent = ({ course }: T_Props) => {
 						<AlignJustify className='h-5 w-5' />
 					</Toggle>
 				</div>
-				<Button variant='default' className='rounded-md' style={{ backgroundColor: course?.color || '' }}>
+
+				{/* Save */}
+				<Button
+					disabled
+					variant='default'
+					className='rounded-md'
+					style={{ backgroundColor: course?.color || '' }}>
 					<Check className='size-5' /> Save
+				</Button>
+
+				{/* Export */}
+				<Button className='rounded-md' variant='secondary' disabled>
+					<FileOutput className='size-5' /> Export PDF
 				</Button>
 			</div>
 
-			<div className='relative mt-4 flex-1 overflow-y-scroll scroll-auto leading-loose scrollbar-hide'>
-				<p className='pointer-events-none absolute left-0 top-0 inline-block select-none overflow-hidden text-ellipsis opacity-50'>
+			{/* Text */}
+			<div className='relative flex-1 overflow-y-scroll scroll-auto p-4 leading-loose scrollbar-hide'>
+				<p className='pointer-events-none absolute left-4 top-4 inline-block select-none overflow-hidden text-ellipsis opacity-50'>
 					Go to the sidebar to create a new note or select existing one to continue
 				</p>
 			</div>
