64cac41bd1db0c892eba3faf903234224b9b179d
Mateusz Mańczak <mateuszmanczak@icloud.com>
2025-02-08 14:01:01 +0100

    chore: remove language setting from settings

--- src/app/settings/_components/settings-page.tsx
+++ src/app/settings/_components/settings-page.tsx
@@ -9,7 +9,6 @@ import { LogOut } from 'lucide-react';
 import ChangeEmailSetting from './change-email-setting';
 import ChangePasswordSetting from './change-password-setting';
 import EmailNotConfirmed from './email-not-confirmed';
-import UpdateSettings from './update-settings';
 
 const SettingsPage = () => {
 	const { data: user, isPending, error } = useUser();
@@ -22,15 +21,8 @@ const SettingsPage = () => {
 		<main className='mx-auto mb-32 mt-4 flex max-w-[480px] flex-col gap-8'>
 			<h1 className='text-3xl font-bold'>Settings</h1>
 			{!user.emailVerified && <EmailNotConfirmed emailConfirmed={!!user.emailVerified} email={user.email} />}
-
-			{/* Change email */}
 			<ChangeEmailSetting />
-
-			{/* Change password */}
 			<ChangePasswordSetting />
-
-			{/* General settings */}
-			<UpdateSettings />
 
 			{/* Logout button */}
 			<Button variant='secondary' onClick={logout} className='w-full'>
