dc022d81899982e1615d311f5198f09136e52e4d
Mateusz Mańczak <mateuszmanczak@icloud.com>
2025-02-09 15:33:53 +0100

    fix: remove overflow hidden for note tasks

--- src/app/notes/_components/sidebar/note-tasks/note-task-item.tsx
+++ src/app/notes/_components/sidebar/note-tasks/note-task-item.tsx
@@ -18,8 +18,8 @@ const NoteTaskItem = ({ task }: T_Props) => {
 		<Reorder.Item
 			value={task}
 			whileDrag={{ userSelect: 'none', pointerEvents: 'none' }}
-			className='mt-2 flex w-full cursor-grab gap-2 overflow-hidden rounded-md border border-neutral-200 bg-white first-of-type:mt-0 dark:border-neutral-700 dark:bg-neutral-800'>
-			<div className='grid h-auto w-6 cursor-move place-content-center bg-neutral-100 dark:bg-neutral-700'>
+			className='mt-2 flex w-full cursor-grab gap-2 rounded-md border border-neutral-200 bg-white first-of-type:mt-0 dark:border-neutral-700 dark:bg-neutral-800'>
+			<div className='grid h-auto w-6 cursor-move place-content-center rounded-l-sm bg-neutral-100 dark:bg-neutral-700'>
 				<GripVertical className='size-4' />
 			</div>
 			<div className='flex min-w-0 flex-1 select-none flex-col p-2'>
