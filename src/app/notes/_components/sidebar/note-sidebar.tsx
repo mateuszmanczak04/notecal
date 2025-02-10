301b8d82548e295e12238360c02234d5f6c9fb5d
Mateusz Mańczak <mateuszmanczak@icloud.com>
2025-02-10 20:59:30 +0100

    feat: showing and hiding note sidebar

--- src/app/notes/_components/sidebar/note-sidebar.tsx
+++ src/app/notes/_components/sidebar/note-sidebar.tsx
@@ -1,7 +1,9 @@
 'use client';
 
 import { useSettings } from '@/hooks/use-settings';
+import { cn } from '@/utils/cn';
 import { Course as T_Course, Note as T_Note } from '@prisma/client';
+import { Menu } from 'lucide-react';
 import CourseRelated from './course-related/course-related';
 import CourseUsefulLinks from './course-useful-links';
 import CustomizeSidebar from './customize-sidebar';
@@ -18,31 +20,43 @@ type T_Props = {
 };
 
 const NoteSidebar = ({ course, currentNote }: T_Props) => {
-	const { sidebarElements } = useSettings();
-
+	const { sidebarElements, setShowNoteSidebar, showNoteSidebar } = useSettings();
 	const { handleMouseDown, handleMouseUp, sidebarRef, noteSidebarWidth } = useResizeSidebar();
 
 	return (
-		<aside
-			ref={sidebarRef}
-			className='relative flex h-screen w-full shrink-0 flex-col overflow-y-scroll bg-white pb-32 scrollbar-hide md:w-72 lg:w-80 xl:w-72 2xl:w-96 dark:border-neutral-600 dark:bg-neutral-900'
-			style={{
-				width: noteSidebarWidth,
-			}}>
-			{sidebarElements.courseRelated && <CourseRelated course={course} />}
-			{sidebarElements.notesList && <SideNotes currentCourse={course} />}
-			{sidebarElements.usefulLinks && <CourseUsefulLinks course={course} />}
-			{sidebarElements.tasks && <NoteTasks course={course} />}
-			{/* TODO: fix useDatePickerFunctionality to not be fired on every click outside */}
-			{sidebarElements.noteRelated && currentNote && <NoteRelated note={currentNote} />}
-			{sidebarElements.settings && <NotesSettings />}
-			{sidebarElements.dangerZone && <NoteDangerZone course={course} />}
-			<CustomizeSidebar course={course} />
-			<div
-				className='absolute left-0 h-full w-1 cursor-ew-resize bg-neutral-200 dark:bg-neutral-700'
-				onMouseDown={handleMouseDown}
-				onMouseUp={handleMouseUp}></div>
-		</aside>
+		<>
+			{showNoteSidebar && (
+				<aside
+					ref={sidebarRef}
+					className='relative flex h-screen w-full shrink-0 flex-col overflow-y-scroll bg-white pb-32 scrollbar-hide md:w-72 lg:w-80 xl:w-72 2xl:w-96 dark:border-neutral-600 dark:bg-neutral-900'
+					style={{
+						width: noteSidebarWidth,
+					}}>
+					{sidebarElements.courseRelated && <CourseRelated course={course} />}
+					{sidebarElements.notesList && <SideNotes currentCourse={course} />}
+					{sidebarElements.usefulLinks && <CourseUsefulLinks course={course} />}
+					{sidebarElements.tasks && <NoteTasks course={course} />}
+					{/* TODO: fix useDatePickerFunctionality to not be fired on every click outside */}
+					{sidebarElements.noteRelated && currentNote && <NoteRelated note={currentNote} />}
+					{sidebarElements.settings && <NotesSettings />}
+					{sidebarElements.dangerZone && <NoteDangerZone course={course} />}
+					<CustomizeSidebar course={course} />
+					<div
+						className='absolute left-0 h-full w-1 cursor-ew-resize bg-neutral-200 dark:bg-neutral-700'
+						onMouseDown={handleMouseDown}
+						onMouseUp={handleMouseUp}></div>
+				</aside>
+			)}
+			<button
+				onClick={() => setShowNoteSidebar(prev => !prev)}
+				className={cn(
+					'fixed right-0 top-0 grid size-[53px] cursor-pointer place-content-center bg-white dark:bg-neutral-800',
+					!showNoteSidebar && 'border-b border-neutral-200 dark:border-neutral-600 ',
+					showNoteSidebar && 'rounded-bl-xl shadow-xl dark:bg-neutral-800',
+				)}>
+				<Menu />
+			</button>
+		</>
 	);
 };
 
