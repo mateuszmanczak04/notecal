718dc79639704a46c29716c8aedead16bd5103dd
Mateusz Mańczak <mateuszmanczak@icloud.com>
2025-02-10 17:52:39 +0100

    feat(editor): transforming $$ into math-node

--- src/app/notes/_components/editor/editor-config.ts
+++ src/app/notes/_components/editor/editor-config.ts
@@ -5,6 +5,7 @@ import { InitialConfigType } from '@lexical/react/LexicalComposer';
 import { HorizontalRuleNode } from '@lexical/react/LexicalHorizontalRuleNode';
 import { HeadingNode, QuoteNode } from '@lexical/rich-text';
 import { TableCellNode, TableNode, TableRowNode } from '@lexical/table';
+import MathNode from './math/math-node';
 
 /** A config for lexical library (WYSIWYG text editor). */
 export const editorConfig: InitialConfigType = {
@@ -23,6 +24,7 @@ export const editorConfig: InitialConfigType = {
 		LinkNode,
 		AutoLinkNode,
 		HorizontalRuleNode,
+		MathNode,
 	],
 	onError(error: Error) {
 		throw error;
