9cce4a35b770da8dc371134c34db7500770dfa03
Mateusz Mańczak <mateuszmanczak@icloud.com>
2025-02-14 12:16:40 +0100

    fix: rendering courses page

--- src/app/courses/_components/courses-page.tsx
+++ src/app/courses/_components/courses-page.tsx
@@ -1,34 +1,11 @@
-'use client';
-
-import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from '@/components/dialog';
-import { Plus } from 'lucide-react';
-import { useState } from 'react';
 import Courses from './courses';
-import CreateCourseForm from './create-course-form';
+import CreateCourseDialog from './create-course-dialog';
 
 const CoursesPage = () => {
-	const [isOpen, setIsOpen] = useState(false);
-
 	return (
 		<main className='grid gap-2 p-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5'>
-			{/* List of courses */}
 			<Courses />
-
-			{/* Create course trigger and dialog */}
-			<Dialog onOpenChange={setIsOpen} open={isOpen}>
-				<DialogTrigger asChild>
-					{/* New course button link */}
-					<button className='grid cursor-pointer place-content-center gap-2 rounded-xl bg-neutral-100 p-4 transition hover:bg-neutral-200 dark:bg-neutral-700 dark:text-neutral-100 dark:hover:bg-neutral-600'>
-						<Plus className='h-10 w-10' />
-					</button>
-				</DialogTrigger>
-				<DialogContent>
-					<DialogHeader>
-						<DialogTitle>Create new course</DialogTitle>
-					</DialogHeader>
-					<CreateCourseForm handleCloseModal={() => setIsOpen(false)} />
-				</DialogContent>
-			</Dialog>
+			<CreateCourseDialog />
 		</main>
 	);
 };
--- src/app/courses/_components/courses.tsx
+++ src/app/courses/_components/courses.tsx
@@ -1,12 +1,42 @@
 'use client';
 
 import { useCourses } from '@/hooks/use-courses';
+import { ChevronRight } from 'lucide-react';
+import { useEffect, useState } from 'react';
 import Course from './course';
 
-const Courses = () => {
-	const { data: courses } = useCourses();
+export const CoursesSkeleton = () => {
+	return (
+		<>
+			{[...Array(10)].map((_, index) => (
+				<div
+					key={index}
+					className='flex animate-pulse cursor-pointer select-none items-center justify-between rounded-xl bg-neutral-100 p-4 text-white transition hover:opacity-90 dark:bg-neutral-700'>
+					<div className='flex-1 overflow-x-hidden '>
+						<p className='h-6 w-full truncate rounded-md bg-neutral-200 text-xl font-medium dark:bg-neutral-600'></p>
+						<p className='mt-1 h-4 truncate rounded-md bg-neutral-200 opacity-75 dark:bg-neutral-600'></p>
+					</div>
+					<ChevronRight className='h-10 w-10 text-neutral-300 dark:text-neutral-500' />
+				</div>
+			))}
+		</>
+	);
+};
 
-	// Should not occur in normal app conditions
+const Courses = () => {
+	const { data: courses, isPending } = useCourses();
+	const [isClient, setIsClient] = useState(false);
+
+	// This is a workaround to prevent the component from rendering on the server.
+	// Without it we could get a hydration mismatch warning.
+	useEffect(() => {
+		setIsClient(true);
+	}, []);
+
+	if (!isClient) return;
+
+	if (isPending) return <CoursesSkeleton />;
+
 	if (!courses) return;
 
 	return courses.map(course => <Course course={course} key={course.id} />);
