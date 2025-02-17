92dd5c8ba4b381347f838299f80741a59b6b17fc
Mateusz Mańczak <mateuszmanczak@icloud.com>
2025-02-17 12:46:52 +0100

    chore: create aws signed urls util functions

---
+++ src/utils/aws.ts
@@ -0,0 +1,41 @@
+'server-only';
+
+import { GetObjectCommand, PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
+
+export const generatePutPresignedUrl = async (noteId: string) => {
+	const client = new S3Client({
+		region: 'us-central-1',
+		credentials: {
+			accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+			secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+		},
+	});
+
+	const command = new PutObjectCommand({
+		Bucket: 'notecal',
+		Key: `notes/${noteId}`,
+		ContentType: 'application/json',
+	});
+
+	const url = await getSignedUrl(client, command, { expiresIn: 3600 });
+	return url;
+};
+
+export const generateGetPresignedUrl = async (noteId: string) => {
+	const client = new S3Client({
+		region: 'us-central-1',
+		credentials: {
+			accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+			secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+		},
+	});
+
+	const command = new GetObjectCommand({
+		Bucket: 'notecal',
+		Key: `notes/${noteId}`,
+	});
+
+	const url = await getSignedUrl(client, command, { expiresIn: 3600 });
+	return url;
+};
