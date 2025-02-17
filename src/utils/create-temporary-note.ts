b7c54985d1beeb8a09a0b2fdad6766d5646b4dce
Mateusz Mańczak <mateuszmanczak@icloud.com>
2025-02-17 14:14:40 +0100

    fix: remove content property from createTemporaryNote

--- src/utils/create-temporary-note.ts
+++ src/utils/create-temporary-note.ts
@@ -1,12 +1,10 @@
 import { T_CreateNoteInput } from '@/app/notes/_actions/create-note';
 import { Note as T_Note } from '@prisma/client';
 import { addMinutes } from 'date-fns';
-import { getDefaultNewNoteContent } from './get-default-new-note-content';
 
 export const createTemporaryNote = ({ courseId, duration, startTime }: T_CreateNoteInput): T_Note => {
 	return {
 		id: 'temp',
-		content: getDefaultNewNoteContent(),
 		courseId,
 		createdAt: new Date(),
 		updatedAt: new Date(),
