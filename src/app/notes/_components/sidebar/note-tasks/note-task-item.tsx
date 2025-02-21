be8e8eb124c28560b15ceeb96b897032a5e716b8
Mateusz Mańczak <mateuszmanczak@icloud.com>
2025-02-21 14:16:58 +0100

    chore: limit note tasks drag anchor to side part

--- src/app/notes/_components/sidebar/note-tasks/note-task-item.tsx
+++ src/app/notes/_components/sidebar/note-tasks/note-task-item.tsx
@@ -1,6 +1,8 @@
+'use client';
+
 import { Task as T_Task, type Task as NoteTaskItem } from '@prisma/client';
 import { GripVertical } from 'lucide-react';
-import { Reorder } from 'motion/react';
+import { Reorder, useDragControls } from 'motion/react';
 import NoteTaskCompleted from './note-task-completed';
 import NoteTaskCourse from './note-task-course';
 import NoteTaskDescription from './note-task-description';
@@ -14,12 +16,18 @@ type T_Props = {
 
 /** Single task used in /notes page */
 const NoteTaskItem = ({ task }: T_Props) => {
+	const dragControls = useDragControls();
+
 	return (
 		<Reorder.Item
 			value={task}
+			dragListener={false}
+			dragControls={dragControls}
 			whileDrag={{ userSelect: 'none', pointerEvents: 'none' }}
 			className='mt-2 flex w-full cursor-grab gap-2 rounded-md border border-neutral-200 bg-white first-of-type:mt-0 dark:border-neutral-700 dark:bg-neutral-800'>
-			<div className='grid h-auto w-6 cursor-move place-content-center rounded-l-sm bg-neutral-100 dark:bg-neutral-700'>
+			<div
+				className='grid h-auto w-6 cursor-move place-content-center rounded-l-sm bg-neutral-100 dark:bg-neutral-700'
+				onPointerDown={e => dragControls.start(e)}>
 				<GripVertical className='size-4' />
 			</div>
 			<div className='flex min-w-0 flex-1 select-none flex-col p-2'>
