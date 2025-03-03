1144f7814761f70004db94781c71ea6592fa2fc2
Mateusz Mańczak <mateuszmanczak@icloud.com>
2025-03-03 15:07:16 +0100

    Rewrite changing password into api handlers

--- src/app/settings/_components/change-password-setting.tsx
+++ src/app/settings/_components/change-password-setting.tsx
@@ -4,24 +4,59 @@ import { Button } from '@/components/button';
 import ErrorMessage from '@/components/error-message';
 import FormLoadingSpinner from '@/components/form-loading-spinner';
 import { Input } from '@/components/input';
+import LoadingSpinner from '@/components/loading-spinner';
 import SuccessMessage from '@/components/success-message';
-import { useActionState } from 'react';
-import changePassword from '../_actions/change-password';
+import { useRef, useState, useTransition } from 'react';
 
 const ChangePasswordSetting = () => {
-	const [state, formAction] = useActionState(changePassword, { message: '' });
+	const [error, setError] = useState('');
+	const [message, setMessage] = useState('');
+	const [isPending, startTransition] = useTransition();
+	const [oldPassword, setOldPassword] = useState('');
+	const [newPassword, setNewPassword] = useState('');
+	const formRef = useRef<HTMLFormElement>(null!);
+
+	const handleSubmit = (e: React.FormEvent) => {
+		e.preventDefault();
+		startTransition(async () => {
+			setError('');
+			setMessage('');
+			const res = await fetch('/api/auth/update-password', {
+				method: 'PUT',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({ oldPassword, newPassword }),
+			}).then(res => res.json());
+			if ('error' in res) {
+				setError(res.error);
+			}
+			if ('message' in res) {
+				setMessage(res.message);
+				formRef.current.reset();
+			}
+		});
+	};
 
 	return (
 		<section className='flex flex-col gap-2'>
 			<h2 className='text-lg font-semibold'>Change your password</h2>
 
-			<form action={formAction} className='mt-2 flex flex-col gap-4'>
+			<form onSubmit={handleSubmit} ref={formRef} className='mt-2 flex flex-col gap-4'>
 				{/* Old password field */}
 				<div>
 					<label htmlFor='change-password-old' className='mb-1 block px-2 font-medium'>
 						Old password
 					</label>
-					<Input placeholder='******' type='password' name='oldPassword' id='change-password-old' required />
+					<Input
+						placeholder='******'
+						type='password'
+						name='oldPassword'
+						id='change-password-old'
+						required
+						value={oldPassword}
+						onChange={e => setOldPassword(e.target.value)}
+					/>
 				</div>
 
 				{/* New password field */}
@@ -29,17 +64,25 @@ const ChangePasswordSetting = () => {
 					<label htmlFor='change-password-new' className='mb-1 block px-2 font-medium'>
 						New password
 					</label>
-					<Input placeholder='******' type='password' name='newPassword' id='change-password-new' required />
+					<Input
+						placeholder='******'
+						type='password'
+						name='newPassword'
+						id='change-password-new'
+						required
+						value={newPassword}
+						onChange={e => setNewPassword(e.target.value)}
+					/>
 				</div>
 
 				{/* Submit button */}
 				<Button type='submit' className='gap-2'>
+					{isPending && <LoadingSpinner />}
 					Save changes
 				</Button>
 
-				{/* Form results */}
-				{state.message && <SuccessMessage>{state.message}</SuccessMessage>}
-				{state.error && <ErrorMessage>{state.error}</ErrorMessage>}
+				{message && <SuccessMessage>{message}</SuccessMessage>}
+				{error && <ErrorMessage>{error}</ErrorMessage>}
 
 				<FormLoadingSpinner />
 			</form>
