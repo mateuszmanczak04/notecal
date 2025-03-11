68c2702e88851ca19bf10db9efcffab121070dfb
Mateusz Mańczak <mateuszmanczak@icloud.com>
2025-03-11 08:42:57 +0100

    Update app domain in production

--- next-app/src/utils/app-domain.ts
+++ next-app/src/utils/app-domain.ts
@@ -1,1 +1,2 @@
-export const FRONTEND_DOMAIN = 'http://localhost:5173';
+export const FRONTEND_DOMAIN =
+	process.env.NODE_ENV === 'development' ? 'http://localhost:5173' : 'https://notecal-client.vercel.app';
