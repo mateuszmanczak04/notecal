693ca682030ab32ce19f69a495289f49039ad01d
Mateusz Mańczak <mateuszmanczak@icloud.com>
2025-03-15 12:47:50 +0100

    Fix some colors

--- vite-app/src/components/input.tsx
+++ vite-app/src/components/input.tsx
@@ -8,7 +8,7 @@ const Input = forwardRef<HTMLInputElement, InputProps>(({ className, type, ...pr
 		<input
 			type={type}
 			className={cn(
-				'border-input placeholder:text-muted-foreground focus-visible:ring-ring flex h-9 w-full rounded-xl border bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none focus-visible:ring-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-neutral-600',
+				'focus-visible:ring-ring flex h-9 w-full rounded-xl border border-neutral-200 bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-neutral-400 focus-visible:outline-none focus-visible:ring-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-neutral-600',
 				className,
 			)}
 			ref={ref}
