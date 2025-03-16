fb4328ac4a94c04ac87a464e14612978ac39237e
Mateusz Mańczak <mateuszmanczak@icloud.com>
2025-03-16 15:57:25 +0100

    Add cursor-pointer

--- vite-app/src/components/checkbox.tsx
+++ vite-app/src/components/checkbox.tsx
@@ -10,11 +10,10 @@ const Checkbox = React.forwardRef<
 	<CheckboxPrimitive.Root
 		ref={ref}
 		className={cn(
-			'focus-visible:ring-ring data-[state=checked]:bg-primary-500 dark:data-[state=checked]:bg-primary-500 peer h-6 w-6 shrink-0 rounded-md border border-neutral-300 bg-neutral-100 focus-visible:outline-none focus-visible:ring-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-transparent dark:bg-neutral-700',
+			'focus-visible:ring-ring data-[state=checked]:bg-primary-500 dark:data-[state=checked]:bg-primary-500 peer h-6 w-6 shrink-0 cursor-pointer rounded-md border border-neutral-300 bg-neutral-100 focus-visible:outline-none focus-visible:ring-1 disabled:cursor-not-allowed disabled:opacity-50 dark:cursor-pointer dark:border-transparent',
 			className,
 		)}
-		{...props}
-	>
+		{...props}>
 		<CheckboxPrimitive.Indicator className={cn('flex items-center justify-center text-white')}>
 			<CheckIcon className='size-5' />
 		</CheckboxPrimitive.Indicator>
