4a1ea6ae43dbc9b16d0f9e76b261209407ce0074
Mateusz Mańczak <mateuszmanczak@icloud.com>
2025-03-27 15:37:07 +0100

    Add error middleware

--- server/index.ts
+++ server/index.ts
@@ -1,18 +1,21 @@
 import express, { type NextFunction, type Request, type Response } from 'express';
 const app = express();
 
-const authMiddleware = (_req: Request, res: Response, next: NextFunction) => {
+const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
 	const authenticated = true;
 	if (!authenticated) {
-		res.status(401).end()
-        return
+		res.status(401).end();
+		return;
 	}
 	next();
 };
 
-
 app.get('/api', authMiddleware, (req, res) => {
 	res.send('Hello World!');
+});
+
+app.use((_err: Error, _req: Request, res: Response, _next: NextFunction) => {
+	res.status(500).json({ error: 'Something went wrong.' });
 });
 
 const PORT = process.env.PORT || 3000;
