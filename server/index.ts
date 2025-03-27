ec7d7ee8e178d71856c383e21d40c0b8a66a9b8a
Mateusz Mańczak <mateuszmanczak@icloud.com>
2025-03-27 10:12:09 +0100

    Install prettier on server, create example authMiddleware

--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,21 @@
-import express from 'express';
+import express, { type NextFunction, type Request, type Response } from 'express';
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send("Hello World!")
-})
+const authMiddleware = (_req: Request, res: Response, next: NextFunction) => {
+	const authenticated = true;
+	if (!authenticated) {
+		res.status(401).end()
+        return
+	}
+	next();
+};
+
+
+app.get('/api', authMiddleware, (req, res) => {
+	res.send('Hello World!');
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`)
-})
+	console.log(`Listening on port ${PORT}`);
+});
