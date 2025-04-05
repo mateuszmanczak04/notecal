1a869a8a7b5fc557eb688c7e2f46e6a6688ecf88
Mateusz Mańczak <mateuszmanczak@icloud.com>
2025-04-05 13:39:55 +0200

    Remove unused note

--- client/src/components/notes/components/context-menu/use-export-note.ts
+++ client/src/components/notes/components/context-menu/use-export-note.ts
@@ -2,7 +2,7 @@ import { useMutation } from '@tanstack/react-query';
 import { T_Note } from '../../../../types';
 import { useToast } from '../../../toast/use-toast';
 
-export const useExportNote = ({ note, onSettledCallback }: { note: T_Note; onSettledCallback?: () => void }) => {
+export const useExportNote = ({ onSettledCallback }: { note: T_Note; onSettledCallback?: () => void }) => {
 	const { toast } = useToast();
 
 	return useMutation({
