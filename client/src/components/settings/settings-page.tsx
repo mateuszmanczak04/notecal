6381d9a32fd5c114b81544c57ba6775be29c3025
Mateusz Mańczak <mateuszmanczak@icloud.com>
2025-04-08 18:10:04 +0200

    Fix settings page padding

--- client/src/components/settings/settings-page.tsx
+++ client/src/components/settings/settings-page.tsx
@@ -5,9 +5,8 @@ import LogoutButton from './components/logout-button';
 
 const SettingsPage = () => {
 	return (
-		<main className='mx-auto mb-32 mt-8 flex max-w-lg flex-col gap-8'>
+		<main className='mx-auto flex max-w-lg flex-col gap-8 p-4 lg:mt-8'>
 			<title>Settings</title>
-			<h1 className='text-3xl font-bold'>Settings</h1>
 			<EmailNotVerified />
 			<ChangeEmailSetting />
 			<ChangePasswordSetting />
