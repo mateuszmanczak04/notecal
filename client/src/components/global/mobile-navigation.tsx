c823592385cb54964ff01965be9874427151dfc3
Mateusz Mańczak <mateuszmanczak@icloud.com>
2025-04-08 18:12:37 +0200

    Highlight currently active tab on mobile nav

--- client/src/components/global/mobile-navigation.tsx
+++ client/src/components/global/mobile-navigation.tsx
@@ -1,19 +1,36 @@
 import { Calendar, Check, GraduationCap, Settings } from 'lucide-react';
 import { NavLink } from 'react-router';
+import { cn } from '../../utils/cn';
 
 const MobileNavigation = () => {
 	return (
 		<div className='grid w-screen grid-cols-4 bg-neutral-100 shadow-2xl lg:hidden dark:bg-neutral-800'>
-			<NavLink to='/calendar' className='grid place-content-center p-4'>
+			<NavLink
+				to='/calendar'
+				className={({ isActive }) =>
+					cn('grid place-content-center p-4', isActive && 'bg-white dark:bg-neutral-900')
+				}>
 				<Calendar className='h-6 w-6 shrink-0' />
 			</NavLink>
-			<NavLink to='/tasks' className='grid place-content-center p-4'>
+			<NavLink
+				to='/tasks'
+				className={({ isActive }) =>
+					cn('grid place-content-center p-4', isActive && 'bg-white dark:bg-neutral-900')
+				}>
 				<Check className='h-6 w-6 shrink-0' />
 			</NavLink>
-			<NavLink to='/courses' className='grid place-content-center p-4'>
+			<NavLink
+				to='/courses'
+				className={({ isActive }) =>
+					cn('grid place-content-center p-4', isActive && 'bg-white dark:bg-neutral-900')
+				}>
 				<GraduationCap className='h-6 w-6 shrink-0' />
 			</NavLink>
-			<NavLink to='/settings' className='grid place-content-center p-4'>
+			<NavLink
+				to='/settings'
+				className={({ isActive }) =>
+					cn('grid place-content-center p-4', isActive && 'bg-white dark:bg-neutral-900')
+				}>
 				<Settings className='h-6 w-6 shrink-0' />
 			</NavLink>
 		</div>
